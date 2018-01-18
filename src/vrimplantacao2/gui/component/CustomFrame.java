d9b39ce8ecc46fedb5f76e076e228a654161b21f
Leandro de Paula Caires <leandrodepaulacaires@gmail.com>
2018-01-18 09:31:20 -0200

    *** Correção - Ajuste no método onDefaultCloseOperation

--- src/vrimplantacao2/gui/component/CustomFrame.java
+++ src/vrimplantacao2/gui/component/CustomFrame.java
@@ -80,7 +80,7 @@ public class CustomFrame extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
