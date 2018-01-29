00058219bfb7e7fb0d704cf526a88f53b029473a
Leandro de Paula Caires <leandrodepaulacaires@gmail.com>
2018-01-29 09:42:05 -0200

    ***Melhoria - Inclusão da importação de mercadológico no sistema Super.

--- src/vrimplantacao2/dao/interfaces/SuperDAO.java
+++ src/vrimplantacao2/dao/interfaces/SuperDAO.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import vrimplantacao.classe.ConexaoFirebird;
 import vrimplantacao2.dao.cadastro.Estabelecimento;
+import vrimplantacao2.vo.importacao.MercadologicoIMP;
 
 public class SuperDAO extends InterfaceDAO {
 
@@ -33,5 +34,57 @@ public class SuperDAO extends InterfaceDAO {
         }
         return result;
     }
+
+    @Override
+    public List<MercadologicoIMP> getMercadologicos() throws Exception {
+        List<MercadologicoIMP> result = new ArrayList<>();
+        
+        try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "select\n" +
+                    "    seg.cd_segmt,\n" +
+                    "    seg.dsc_segmt,\n" +
+                    "    d.cd_depart,\n" +
+                    "    d.dsc_depart,\n" +
+                    "    g.cd_grupo,\n" +
+                    "    g.dsc_grupo,\n" +
+                    "    sb.cd_subgrupo,\n" +
+                    "    sb.dsc_subgrupo\n" +
+                    "from\n" +
+                    "    subgrupo sb\n" +
+                    "    join grupo g on\n" +
+                    "        sb.cd_grupo = g.cd_grupo\n" +
+                    "    join secao s on\n" +
+                    "        g.cd_secao = s.cd_secao\n" +
+                    "    join departamento d on\n" +
+                    "        s.cd_depart = d.cd_depart\n" +
+                    "    join segmento seg on\n" +
+                    "        d.cd_segmt = seg.cd_segmt\n" +
+                    "order by\n" +
+                    "    1,3,5,7"
+            )) {
+                while (rst.next()) {
+                    MercadologicoIMP imp = new MercadologicoIMP();
+                    
+                    imp.setImportSistema(getSistema());
+                    imp.setImportLoja(getLojaOrigem());
+                    imp.setMerc1ID(rst.getString("cd_segmt"));
+                    imp.setMerc1Descricao(rst.getString("dsc_segmt"));
+                    imp.setMerc2ID(rst.getString("cd_depart"));
+                    imp.setMerc2Descricao(rst.getString("dsc_depart"));
+                    imp.setMerc3ID(rst.getString("cd_grupo"));
+                    imp.setMerc3Descricao(rst.getString("dsc_grupo"));
+                    imp.setMerc4ID(rst.getString("cd_subgrupo"));
+                    imp.setMerc4Descricao(rst.getString("dsc_subgrupo"));
+                    
+                    result.add(imp);
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    
     
 }
