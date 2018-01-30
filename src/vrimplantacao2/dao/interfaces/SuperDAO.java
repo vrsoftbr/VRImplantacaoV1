5ae1bd99b63cfe6bd03ffa65acf35d21c209a352
Leandro de Paula Caires <leandrodepaulacaires@gmail.com>
2018-01-30 10:22:59 -0200

    ***Correção - Executada uma correção na importação de Mercadológico.

--- src/vrimplantacao2/dao/interfaces/SuperDAO.java
+++ src/vrimplantacao2/dao/interfaces/SuperDAO.java
@@ -49,10 +49,8 @@ public class SuperDAO extends InterfaceDAO {
         try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
             try (ResultSet rst = stm.executeQuery(
                     "select\n" +
-                    "    seg.cd_segmt,\n" +
-                    "    seg.dsc_segmt,\n" +
-                    "    d.cd_depart,\n" +
-                    "    d.dsc_depart,\n" +
+                    "    s.cd_secao,\n" +
+                    "    s.dsc_secao,\n" +
                     "    g.cd_grupo,\n" +
                     "    g.dsc_grupo,\n" +
                     "    sb.cd_subgrupo,\n" +
@@ -63,26 +61,20 @@ public class SuperDAO extends InterfaceDAO {
                     "        sb.cd_grupo = g.cd_grupo\n" +
                     "    join secao s on\n" +
                     "        g.cd_secao = s.cd_secao\n" +
-                    "    join departamento d on\n" +
-                    "        s.cd_depart = d.cd_depart\n" +
-                    "    join segmento seg on\n" +
-                    "        d.cd_segmt = seg.cd_segmt\n" +
                     "order by\n" +
-                    "    1,3,5,7"
+                    "    1,3,5"
             )) {
                 while (rst.next()) {
                     MercadologicoIMP imp = new MercadologicoIMP();
                     
                     imp.setImportSistema(getSistema());
                     imp.setImportLoja(getLojaOrigem());
-                    imp.setMerc1ID(rst.getString("cd_segmt"));
-                    imp.setMerc1Descricao(rst.getString("dsc_segmt"));
-                    imp.setMerc2ID(rst.getString("cd_depart"));
-                    imp.setMerc2Descricao(rst.getString("dsc_depart"));
-                    imp.setMerc3ID(rst.getString("cd_grupo"));
-                    imp.setMerc3Descricao(rst.getString("dsc_grupo"));
-                    imp.setMerc4ID(rst.getString("cd_subgrupo"));
-                    imp.setMerc4Descricao(rst.getString("dsc_subgrupo"));
+                    imp.setMerc1ID(rst.getString("cd_secao"));
+                    imp.setMerc1Descricao(rst.getString("dsc_secao"));
+                    imp.setMerc2ID(rst.getString("cd_grupo"));
+                    imp.setMerc2Descricao(rst.getString("dsc_grupo"));
+                    imp.setMerc3ID(rst.getString("cd_subgrupo"));
+                    imp.setMerc3Descricao(rst.getString("dsc_subgrupo"));
                     
                     result.add(imp);
                 }
@@ -169,11 +161,9 @@ public class SuperDAO extends InterfaceDAO {
                     imp.setDescricaoCompleta(rst.getString("descricaocompleta"));
                     imp.setDescricaoGondola(rst.getString("descricaocompleta"));
                     imp.setDescricaoReduzida(rst.getString("descricaoreduzida"));
-                    imp.setCodMercadologico1(rst.getString("cd_segmt"));
-                    imp.setCodMercadologico2(rst.getString("cd_depart"));
-                    imp.setCodMercadologico3(rst.getString("cd_secao"));
-                    imp.setCodMercadologico4(rst.getString("cd_grupo"));
-                    imp.setCodMercadologico5(rst.getString("cd_sub_grupo"));
+                    imp.setCodMercadologico1(rst.getString("cd_secao"));
+                    imp.setCodMercadologico2(rst.getString("cd_grupo"));
+                    imp.setCodMercadologico3(rst.getString("cd_sub_grupo"));
                     imp.setPesoBruto(rst.getDouble("peso"));
                     imp.setPesoLiquido(rst.getDouble("peso"));
                     imp.setEstoqueMaximo(rst.getDouble("estoquemaximo"));
