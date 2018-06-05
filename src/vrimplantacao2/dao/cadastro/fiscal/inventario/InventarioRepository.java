3180a6c80406105374b8fe31f76e17048d672b31
Lucas Rafael <lucas.rafael@vrsoft.com.br>
2018-06-05 16:51:02 -0300

    Criação da rotina para importar Inventário sistema Avance (cliente JF)

--- src/vrimplantacao2/dao/cadastro/fiscal/inventario/InventarioRepository.java
+++ src/vrimplantacao2/dao/cadastro/fiscal/inventario/InventarioRepository.java
@@ -33,6 +33,8 @@ public class InventarioRepository {
             //</editor-fold>
             
             setNotificacao("Gravando operador...", inventario.size());
+            
+            //if ()
 
         } catch (Exception ex) {
             throw ex;
