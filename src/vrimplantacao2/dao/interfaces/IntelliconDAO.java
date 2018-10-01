390848a0c48852b06c17b999fdb9315bccba76a8
Guilherme Gomes <guilherme.gomes@vrsoft.com.br>
2018-10-01 14:32:43 -0300

    Inicio desenvolvimento sistema Lyncis.

--- src/vrimplantacao2/dao/interfaces/IntelliconDAO.java
+++ src/vrimplantacao2/dao/interfaces/IntelliconDAO.java
@@ -222,8 +222,15 @@ public class IntelliconDAO extends InterfaceDAO implements MapaTributoProvider {
                     imp.setImportId(rs.getString("id"));
                     imp.setEan(rs.getString("codigobarras"));
                     imp.setDescricaoCompleta(rs.getString("nomecompleto"));
-                    imp.setDescricaoReduzida(rs.getString("nomereduzido"));
-                    imp.setDescricaoGondola(rs.getString("nomereduzido"));
+                    
+                    if((rs.getString("nomereduzido") == null) && ("".equals(rs.getString("nomereduzido")))) {
+                        imp.setDescricaoReduzida(rs.getString("nomecompleto").substring(1, 22));
+                    } else {
+                        imp.setDescricaoReduzida(rs.getString("nomereduzido"));
+                    }
+                        
+                    imp.setDescricaoGondola(rs.getString("nomecompleto"));
+                    
                     imp.setCodMercadologico1(rs.getString("merc1"));
                     imp.setCodMercadologico2(rs.getString("merc2"));
                     imp.setCodMercadologico3(rs.getString("merc3"));
@@ -502,6 +509,7 @@ public class IntelliconDAO extends InterfaceDAO implements MapaTributoProvider {
                     + "where\n"
                     + "    dr.loja = " + getLojaOrigem() + " and\n"
                     + "    dr.pago = 'N' and\n"
+                    + "    dr.num_ecf != 0 and\n"
                     + "    dr.data_cupom between '01.01.2018' and '30.09.2018'\n"
                     + "order by\n"
                     + "    dr.data_cupom")) {
