6f8d521d56180a4cb433132ce8b1151a00d0fb01
Lucas Rafael <lucas.rafael@vrsoft.com.br>
2019-09-16 11:23:40 -0300

    Desenvolvimento/Atualização de versão RootacDAO

---
+++ src/vrimplantacao2/dao/interfaces/RootacDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vrimplantacao2.dao.interfaces;
+
+/**
+ *
+ * @author lucasrafael
+ */
+public class RootacDAO extends InterfaceDAO {
+    
+    @Override
+    public String getSistema() {
+        return "Rootac";
+    }
+}
