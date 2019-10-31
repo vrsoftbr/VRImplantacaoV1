dccf56e8a476ad09a7121dbcb5b715e3cdda83a7
Lucas Rafael <lucas.rafael@vrsoft.com.br>
2019-10-31 12:30:27 -0300

    Desenvolvimento AtmaDAO (getClientes)

--- src/vrimplantacao2/gui/interfaces/AtmaDAO.java
+++ src/vrimplantacao2/gui/interfaces/AtmaDAO.java
@@ -20,7 +20,10 @@ import vrimplantacao2.dao.interfaces.InterfaceDAO;
 import vrimplantacao2.vo.cadastro.mercadologico.MercadologicoNivelIMP;
 import vrimplantacao2.vo.enums.SituacaoCadastro;
 import vrimplantacao2.vo.enums.TipoContato;
+import vrimplantacao2.vo.enums.TipoEstadoCivil;
 import vrimplantacao2.vo.enums.TipoFornecedor;
+import vrimplantacao2.vo.enums.TipoSexo;
+import vrimplantacao2.vo.importacao.ClienteIMP;
 import vrimplantacao2.vo.importacao.CreditoRotativoIMP;
 import vrimplantacao2.vo.importacao.FornecedorIMP;
 import vrimplantacao2.vo.importacao.ProdutoFornecedorIMP;
@@ -316,6 +319,7 @@ public class AtmaDAO extends InterfaceDAO {
                     + "left join dbo.TB_UF ufc on ufc.ID_UF = munc.ID_UF\n"
                     + "left join dbo.TB_MUNICIPIO mune on mune.ID_MUNICIPIO = ende.ID_C_MUNICIPIO\n"
                     + "left join dbo.TB_UF ufe on ufc.ID_UF = mune.ID_UF\n"
+                    + "where pes.ID_TIPO_ENTIDADE is not null\n"
                     + "order by pes.ID_PES"
             )) {
                 while (rst.next()) {
@@ -444,7 +448,7 @@ public class AtmaDAO extends InterfaceDAO {
                     if ((rst.getString("emailfinanceiro") != null)
                             && (!rst.getString("emailfinanceiro").trim().isEmpty())) {
 
-                        imp.addEmail("EMAIL FIN", rst.getString("emailfinanceiro").toLowerCase(), TipoContato.FINANCEIRO);
+                        imp.addEmail("EMAIL FINANCEIRO", rst.getString("emailfinanceiro").toLowerCase(), TipoContato.FINANCEIRO);
                     }
 
                     result.add(imp);
@@ -477,6 +481,275 @@ public class AtmaDAO extends InterfaceDAO {
                 }
             }
         }
+        return result;
+    }
+
+    @Override
+    public List<ClienteIMP> getClientes() throws Exception {
+        List<ClienteIMP> result = new ArrayList<>();
+
+        try (Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "select \n"
+                    + "	pes.ID_PES as id,\n"
+                    + "	sit.DESCRICAO as situacaocadastro,\n"
+                    + "	ent.DESCRICAO as tipofornecedor,\n"
+                    + "	pes.CPF as cpf,\n"
+                    + "	pes.RG as rg,\n"
+                    + "	pes.CNPJ as cnpj,\n"
+                    + "	pes.INSC as inscricaoestaual,\n"
+                    + "	pes.TIPO_FJ as tipoinscricao,\n"
+                    + "	pes.RAZAO as razao,\n"
+                    + "	pes.FANTASIA as fantasia,\n"
+                    + "	ende.N_ENDERECO as endereco,\n"
+                    + "	ende.N_NRO_ENDERECO as numero,\n"
+                    + "	ende.N_COMPLEMENTO as complemento,\n"
+                    + "	ende.N_BAIRRO as bairro,\n"
+                    + "	ende.N_CEP as cep,\n"
+                    + "	mun.ID_IBGE as municipioIbge,\n"
+                    + "	mun.MUNICIPIO as municipio,\n"
+                    + "	uf.ID_IBGE as ufIbge,\n"
+                    + "	uf.UF as uf,\n"
+                    + "	ende.N_FONE as telefone,\n"
+                    + "	ende.N_FAX as fax,\n"
+                    + "	ende.N_CELULAR as celular,\n"
+                    + "	ende.C_ENDERECO as enderecocob,\n"
+                    + "	ende.C_NRO_ENDERECO as numerocob,\n"
+                    + "	ende.C_BAIRRO as bairrocob,\n"
+                    + "	ende.C_CEP as cepcob,\n"
+                    + "	munc.ID_IBGE as municipioIbgeCob,\n"
+                    + "	munc.MUNICIPIO as municpioCob,\n"
+                    + "	ufc.ID_IBGE as ufIbgeCob,\n"
+                    + "	ufc.UF as ufCob,\n"
+                    + "	ende.C_FONE as telefonecob,\n"
+                    + "	ende.C_FAX as faxcob,\n"
+                    + "	ende.C_CELULAR as celularcob,\n"
+                    + "	ende.E_ENDERECO as enderecoent,\n"
+                    + "	ende.E_NRO_ENDERECO as numeroent,\n"
+                    + "	ende.E_COMPLEMENTO as complementoent,\n"
+                    + "	ende.E_BAIRRO as bairroent,\n"
+                    + "	ende.E_CEP as cepent,\n"
+                    + "	mune.ID_IBGE as municipioIbgeEnt,\n"
+                    + "	mune.MUNICIPIO as municpioEnt,\n"
+                    + "	ufe.ID_IBGE as ufIbgeEnt,\n"
+                    + "	ufe.UF as ufEnt,\n"
+                    + "	ende.E_FONE as telefoneent,\n"
+                    + "	ende.E_FAX as faxent,\n"
+                    + "	ende.E_CELULAR as celularent,\n"
+                    + "	ende.N_PONTO_REF as pontoreferencia,\n"
+                    + "	ende.C_PONTO_REF as pontoreferenciacob,\n"
+                    + "	ende.E_PONTO_REF as pontoreferenciaent,\n"
+                    + "	pes.EMAIL as email,\n"
+                    + "	pes.EMAIL_XML as emailXml,\n"
+                    + "	pes.EMAIL_FINANCEIRO as emailfinanceiro,\n"
+                    + "	pes.ESTADO_CIVIL as estadocivil,\n"
+                    + "	pes.VR_SALARIO as salario,\n"
+                    + "	pes.VR_RENDA as renda,\n"
+                    + "	pes.DT_CAD as datacadastro,\n"
+                    + "	pes.DT_NASC as datanascimento,\n"
+                    + "	pes.DT_ADMISSAO as dataadmissao,\n"
+                    + "	pes.DIAS_VISITA as diasvisita,\n"
+                    + "	pes.ENTREGA_PRAZO as prazoentrega,\n"
+                    + "	pes.TRABALHO_LOCAL as empresa,\n"
+                    + "	pes.TRABALHO_FONE as telefonetrabalho,\n"
+                    + "	pes.PAI as nomepai,\n"
+                    + "	pes.MAE as nomemae,\n"
+                    + "	pes.CARGO as cargo,\n"
+                    + "	pes.SEXO as sexo,\n"
+                    + "	pes.OBS as observacao\n"
+                    + "from dbo.FN_PES pes\n"
+                    + "left join dbo.TB_TIPO_SITUACAO sit on sit.ID_TIPO_SITUACAO = pes.ID_TIPO_SITUACAO\n"
+                    + "left join dbo.TB_TIPO_ENTIDADE ent on ent.ID_TIPO_ENTIDADE = pes.ID_TIPO_ENTIDADE\n"
+                    + "left join dbo.FN_PES_END ende on ende.ID_PES = pes.ID_PES\n"
+                    + "left join dbo.TB_MUNICIPIO mun on mun.ID_MUNICIPIO = ende.ID_N_MUNICIPIO\n"
+                    + "left join dbo.TB_UF as uf on uf.ID_UF = mun.ID_UF\n"
+                    + "left join dbo.TB_MUNICIPIO munc on munc.ID_MUNICIPIO = ende.ID_C_MUNICIPIO\n"
+                    + "left join dbo.TB_UF ufc on ufc.ID_UF = munc.ID_UF\n"
+                    + "left join dbo.TB_MUNICIPIO mune on mune.ID_MUNICIPIO = ende.ID_C_MUNICIPIO\n"
+                    + "left join dbo.TB_UF ufe on ufc.ID_UF = mune.ID_UF\n"
+                    + "where pes.ID_TIPO_ENTIDADE is null\n"
+                    + "order by pes.ID_PES"
+            )) {
+                while (rst.next()) {
+                    ClienteIMP imp = new ClienteIMP();
+                    imp.setId(rst.getString("id"));
+                    imp.setRazao(rst.getString("razao"));
+                    imp.setFantasia(rst.getString("fantasia"));
+                    if ((rst.getString("cpf") != null)
+                            && (!rst.getString("cpf").trim().isEmpty())) {
+                        imp.setCnpj(rst.getString("cpf"));
+                    } else {
+                        imp.setCnpj(rst.getString("cnpj"));
+                    }
+
+                    if ((rst.getString("rg") != null)
+                            && (!rst.getString("rg").trim().isEmpty())) {
+                        imp.setInscricaoestadual(rst.getString("rg"));
+                    } else {
+                        imp.setInscricaoestadual(rst.getString("inscricaoestaual"));
+                    }
+
+                    imp.setEndereco(rst.getString("endereco"));
+                    imp.setNumero(rst.getString("numero"));
+                    imp.setComplemento(rst.getString("complemento"));
+                    imp.setCep(rst.getString("cep"));
+                    imp.setBairro(rst.getString("bairro"));
+                    imp.setMunicipio(rst.getString("municipio"));
+                    imp.setMunicipioIBGE(rst.getInt("municipioIbge"));
+                    imp.setUf(rst.getString("uf"));
+                    imp.setUfIBGE(rst.getInt("ufIbge"));
+                    imp.setCobrancaEndereco(rst.getString("enderecocob"));
+                    imp.setCobrancaNumero(rst.getString("numerocob"));
+                    imp.setCobrancaComplemento(rst.getString("complementocob"));
+                    imp.setCobrancaCep(rst.getString("cepcob"));
+                    imp.setCobrancaBairro(rst.getString("bairrocob"));
+                    imp.setCobrancaMunicipio(rst.getString("municpioCob"));
+                    imp.setCobrancaMunicipioIBGE(rst.getInt("municipioIbgeCob"));
+                    imp.setCobrancaUf(rst.getString("ufCob"));
+                    imp.setCobrancaUfIBGE(rst.getInt("ufIbgeCob"));
+                    imp.setDataCadastro(rst.getDate("datacadastro"));
+                    imp.setDataNascimento(rst.getDate("datanascimento"));
+                    imp.setEmpresa(rst.getString("empresa"));
+                    imp.setCargo(rst.getString("cargo"));
+                    imp.setSalario(rst.getDouble("salario"));
+                    imp.setEmpresaTelefone(rst.getString("telefonetrabalho"));
+                    imp.setNomePai(rst.getString("nomepai"));
+                    imp.setNomeMae(rst.getString("nomemae"));
+                    imp.setObservacao(rst.getString("observacao"));
+                    imp.setTelefone(rst.getString("telefone"));
+                    imp.setFax(rst.getString("fax"));
+                    imp.setCelular(rst.getString("celular"));
+                    imp.setEmail(rst.getString("email") == null ? "" : rst.getString("email").toLowerCase());
+
+                    if ((rst.getString("sexo") != null)
+                            && (!rst.getString("sexo").trim().isEmpty())) {
+
+                        if ("FEMININO".equals(rst.getString("sexo"))) {
+                            imp.setSexo(TipoSexo.FEMININO);
+                        } else {
+                            imp.setSexo(TipoSexo.MASCULINO);
+                        }
+                    }
+
+                    if ((rst.getString("estadocivil") != null)
+                            && (!rst.getString("estadocivil").trim().isEmpty())) {
+
+                        if (rst.getString("estadocivil").contains("SOLTE")) {
+                            imp.setEstadoCivil(TipoEstadoCivil.SOLTEIRO);
+                        } else if (rst.getString("estadocivil").contains("CASA")) {
+                            imp.setEstadoCivil(TipoEstadoCivil.CASADO);
+                        } else if (rst.getString("estadocivil").contains("VIU")) {
+                            imp.setEstadoCivil(TipoEstadoCivil.VIUVO);
+                        } else if (rst.getString("estadocivil").contains("DIVOR")) {
+                            imp.setEstadoCivil(TipoEstadoCivil.DIVORCIADO);
+                        } else if (rst.getString("estadocivil").contains("OUTRO")) {
+                            imp.setEstadoCivil(TipoEstadoCivil.OUTROS);
+                        } else {
+                            imp.setEstadoCivil(TipoEstadoCivil.NAO_INFORMADO);
+                        }
+                    }
+
+                    if ("ATIVO".equals(rst.getString("situacaocadastro"))) {
+                        imp.setAtivo(true);
+                    } else if ("INATIVO".equals(rst.getString("situacaocadastro"))) {
+                        imp.setAtivo(false);
+                    } else if ("BLOQUEADO".equals(rst.getString("situacaocadastro"))) {
+                        imp.setBloqueado(true);
+                        imp.setAtivo(true);
+                    } else {
+                        imp.setBloqueado(false);
+                        imp.setAtivo(true);
+                    }
+
+                    if ((rst.getString("telefonecob") != null)
+                            && (!rst.getString("telefonecob").trim().isEmpty())) {
+                        imp.addContato(
+                                "1",
+                                "TELEFONE COB",
+                                rst.getString("telefonecob"),
+                                null,
+                                null
+                        );
+                    }
+                    if ((rst.getString("faxcob") != null)
+                            && (!rst.getString("faxcob").trim().isEmpty())) {
+                        imp.addContato(
+                                "2",
+                                "FAX COB",
+                                rst.getString("faxcob"),
+                                null,
+                                null
+                        );
+                    }
+                    if ((rst.getString("celularcob") != null)
+                            && (!rst.getString("celularcob").trim().isEmpty())) {
+                        imp.addContato(
+                                "3",
+                                "CELULAR COB",
+                                rst.getString("celularcob"),
+                                null,
+                                null
+                        );
+                    }
+
+                    if ((rst.getString("telefoneent") != null)
+                            && (!rst.getString("telefoneent").trim().isEmpty())) {
+                        imp.addContato(
+                                "4",
+                                "TELEFONE ENT",
+                                rst.getString("telefoneent"),
+                                null,
+                                null
+                        );
+                    }
+                    if ((rst.getString("faxent") != null)
+                            && (!rst.getString("faxent").trim().isEmpty())) {
+                        imp.addContato(
+                                "5",
+                                "FAX ENT",
+                                rst.getString("faxent"),
+                                null,
+                                null
+                        );
+                    }
+                    if ((rst.getString("celularent") != null)
+                            && (!rst.getString("celularent").trim().isEmpty())) {
+                        imp.addContato(
+                                "6",
+                                "CELULAR ENT",
+                                rst.getString("celularent"),
+                                null,
+                                null
+                        );
+                    }
+
+                    if ((rst.getString("emailXml") != null)
+                            && (!rst.getString("emailXml").trim().isEmpty())) {
+                        imp.addContato(
+                                "7",
+                                "EMAIL XML",
+                                null,
+                                null,
+                                rst.getString("emailXml").toLowerCase()
+                        );
+                    }
+                    if ((rst.getString("emailfinanceiro") != null)
+                            && (!rst.getString("emailfinanceiro").trim().isEmpty())) {
+                        imp.addContato(
+                                "7",
+                                "EMAIL FINANCEIRO",
+                                null,
+                                null,
+                                rst.getString("emailfinanceiro").toLowerCase()
+                        );
+                    }
+
+                    result.add(imp);
+
+                }
+            }
+        }
+
         return result;
     }
 
