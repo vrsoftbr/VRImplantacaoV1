85a5897e394ff8aff52c93f13fa64b8953fa7df6
Guilherme Gomes <guilherme.gomes@vrsoft.com.br>
2019-11-27 11:12:35 -0300

    Ajuste ControlXDAO.

--- src/vrimplantacao2/dao/interfaces/ControlXDAO.java
+++ src/vrimplantacao2/dao/interfaces/ControlXDAO.java
@@ -25,16 +25,16 @@ public class ControlXDAO extends InterfaceDAO {
     public String getSistema() {
         return "ControlX";
     }
-    
+
     public List<Estabelecimento> getLojasCliente() throws SQLException {
         List<Estabelecimento> result = new ArrayList<>();
         try (Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
             try (ResultSet rs = stm.executeQuery(
-                    "select\n" +
-                    "	nCodigo id,\n" +
-                    "	cDescricao fantasia\n" +
-                    "from\n" +
-                    "	tabFilial"
+                    "select\n"
+                    + "	nCodigo id,\n"
+                    + "	cDescricao fantasia\n"
+                    + "from\n"
+                    + "	tabFilial"
             )) {
                 while (rs.next()) {
                     result.add(new Estabelecimento(rs.getString("id"), rs.getString("fantasia")));
@@ -47,23 +47,23 @@ public class ControlXDAO extends InterfaceDAO {
     @Override
     public List<MercadologicoIMP> getMercadologicos() throws Exception {
         List<MercadologicoIMP> result = new ArrayList<>();
-        try(Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
-            try(ResultSet rs = stm.executeQuery(
-                    "select\n" +
-                    "	distinct\n" +
-                    "	p.grupo cod_merc1,\n" +
-                    "	g.NOME mercadologico1,\n" +
-                    "	p.SubGrupo cod_merc2,\n" +
-                    "	sg.NOME mercadologico2\n" +
-                    "from\n" +
-                    "	tabproduto p\n" +
-                    "join tabgrupo g on p.Grupo = g.GRUPO\n" +
-                    "join TabSubGru sg on p.SubGrupo = sg.ACESSO\n" +
-                    "order by\n" +
-                    "	1, 3")) {
-                while(rs.next()) {
+        try (Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "select\n"
+                    + "	distinct\n"
+                    + "	p.grupo cod_merc1,\n"
+                    + "	g.NOME mercadologico1,\n"
+                    + "	p.SubGrupo cod_merc2,\n"
+                    + "	sg.NOME mercadologico2\n"
+                    + "from\n"
+                    + "	tabproduto p\n"
+                    + "join tabgrupo g on p.Grupo = g.GRUPO\n"
+                    + "join TabSubGru sg on p.SubGrupo = sg.ACESSO\n"
+                    + "order by\n"
+                    + "	1, 3")) {
+                while (rs.next()) {
                     MercadologicoIMP imp = new MercadologicoIMP();
-                    
+
                     imp.setImportLoja(getLojaOrigem());
                     imp.setImportSistema(getSistema());
                     imp.setMerc1ID(rs.getString("cod_merc1"));
@@ -72,7 +72,7 @@ public class ControlXDAO extends InterfaceDAO {
                     imp.setMerc2Descricao(rs.getString("mercadologico2"));
                     imp.setMerc3ID("1");
                     imp.setMerc3Descricao(imp.getMerc2Descricao());
-                    
+
                     result.add(imp);
                 }
             }
@@ -83,66 +83,66 @@ public class ControlXDAO extends InterfaceDAO {
     @Override
     public List<ProdutoIMP> getProdutos() throws Exception {
         List<ProdutoIMP> result = new ArrayList<>();
-        try(Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
-            try(ResultSet rs = stm.executeQuery(
-                    "select\n" +
-                    "	p.acesso id,\n" +
-                    "	p.REFERENCIA ean,\n" +
-                    "	p.descricao descricaocompleta,\n" +
-                    "	case when coalesce(p.descpeq, p.descricao) = ''\n" +
-                    "	then p.descricao else coalesce(p.descpeq, p.descricao) \n" +
-                    "	end descricaoreduzida,\n" +
-                    "	coalesce(p.qtdminima, 0) qtdminima,\n" +
-                    "	coalesce(p.qtdmaxima, 0) qtdmaxima,\n" +
-                    "	e.nQtdAtual estoque,\n" +
-                    "	p.UNIDVENDA unidadevenda,\n" +
-                    "	p.UNIDCOMPRA unidadecompra,\n" +
-                    "	case when p.nQtdEmbalagem = 0 or p.nQtdEmbalagem is null\n" +
-                    "	then 1 else p.nQtdEmbalagem end  qtdembalagem,\n" +
-                    "	p.dDataCad datacadastro,\n" +
-                    "	p.cSituacaoTribFora id_aliquota_debito_nf_fe,\n" +
-                    "	p.cSituacaoTributaria id_aliquota_debito,\n" +
-                    "	p.nIcms_Fora icms_fora_estado,\n" +
-                    "	p.nCST cst,\n" +
-                    "	p.Situacao situacaocadastro,\n" +
-                    "	p.preco,\n" +
-                    "	c.cValor ultimo_preco,\n" +
-                    "	coalesce(p.nCustoNacional, 0) custocomimposto,\n" +
-                    "	coalesce(p.pesobruto, 0) pesobruto,\n" +
-                    "	coalesce(p.pesoliquido, 0) pesoliquido,\n" +
-                    "	p.grupo merc1,\n" +
-                    "	p.subgrupo merc2,\n" +
-                    "	p.cBalanca isBalanca,\n" +
-                    "	p.cValidade validade,\n" +
-                    "	p.cNCM ncm,\n" +
-                    "	coalesce(p.nCest, 0) cest,\n" +
-                    "	p.nCstCofins cst_cofins_debito,\n" +
-                    "	p.nCstCofins_Entrada cst_cofins_credito,\n" +
-                    "	p.nCstPis cst_pis_debito,\n" +
-                    "	p.nCstPis_Entrada cst_pis_credito,\n" +
-                    "	p.nCstNatREC naturezareceita\n" +
-                    "from\n" +
-                    "	tabproduto p\n" +
-                    "left join tabestqatual e on p.acesso = e.nProduto\n" +
-                    "left join (select \n" +
-                    "			    nacesso,\n" +
-                    "				cvalor,\n" +
-                    "				nproduto \n" +
-                    "			from \n" +
-                    "				tabcustos) c on p.acesso = c.nProduto\n" +
-                    "where\n" +
-                    "	c.nAcesso in (select \n" +
-                    "					max(nAcesso) \n" +
-                    "				  from \n" +
-                    "					tabcustos \n" +
-                    "				  where \n" +
-                    "					tabcustos.nproduto = p.acesso)\n" +
-                    "order by\n" +
-                    "	1"
+        try (Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "select\n"
+                    + "	p.acesso id,\n"
+                    + "	p.REFERENCIA ean,\n"
+                    + "	p.descricao descricaocompleta,\n"
+                    + "	case when coalesce(p.descpeq, p.descricao) = ''\n"
+                    + "	then p.descricao else coalesce(p.descpeq, p.descricao) \n"
+                    + "	end descricaoreduzida,\n"
+                    + "	coalesce(p.qtdminima, 0) qtdminima,\n"
+                    + "	coalesce(p.qtdmaxima, 0) qtdmaxima,\n"
+                    + "	e.nQtdAtual estoque,\n"
+                    + "	p.UNIDVENDA unidadevenda,\n"
+                    + "	p.UNIDCOMPRA unidadecompra,\n"
+                    + "	case when p.nQtdEmbalagem = 0 or p.nQtdEmbalagem is null\n"
+                    + "	then 1 else p.nQtdEmbalagem end  qtdembalagem,\n"
+                    + "	p.dDataCad datacadastro,\n"
+                    + "	p.cSituacaoTribFora id_aliquota_debito_nf_fe,\n"
+                    + "	p.cSituacaoTributaria id_aliquota_debito,\n"
+                    + "	p.nIcms_Fora icms_fora_estado,\n"
+                    + "	p.nCST cst,\n"
+                    + "	p.Situacao situacaocadastro,\n"
+                    + "	p.preco,\n"
+                    + "	c.cValor ultimo_preco,\n"
+                    + "	coalesce(p.nCustoNacional, 0) custocomimposto,\n"
+                    + "	coalesce(p.pesobruto, 0) pesobruto,\n"
+                    + "	coalesce(p.pesoliquido, 0) pesoliquido,\n"
+                    + "	p.grupo merc1,\n"
+                    + "	p.subgrupo merc2,\n"
+                    + "	p.cBalanca isBalanca,\n"
+                    + "	p.cValidade validade,\n"
+                    + "	p.cNCM ncm,\n"
+                    + "	coalesce(p.nCest, 0) cest,\n"
+                    + "	p.nCstCofins cst_cofins_debito,\n"
+                    + "	p.nCstCofins_Entrada cst_cofins_credito,\n"
+                    + "	p.nCstPis cst_pis_debito,\n"
+                    + "	p.nCstPis_Entrada cst_pis_credito,\n"
+                    + "	p.nCstNatREC naturezareceita\n"
+                    + "from\n"
+                    + "	tabproduto p\n"
+                    + "left join tabestqatual e on p.acesso = e.nProduto\n"
+                    + "left join (select \n"
+                    + "			    nacesso,\n"
+                    + "				cvalor,\n"
+                    + "				nproduto \n"
+                    + "			from \n"
+                    + "				tabcustos) c on p.acesso = c.nProduto\n"
+                    + "where\n"
+                    + "	c.nAcesso in (select \n"
+                    + "					max(nAcesso) \n"
+                    + "				  from \n"
+                    + "					tabcustos \n"
+                    + "				  where \n"
+                    + "					tabcustos.nproduto = p.acesso)\n"
+                    + "order by\n"
+                    + "	1"
             )) {
-                while(rs.next()) {
+                while (rs.next()) {
                     ProdutoIMP imp = new ProdutoIMP();
-                    
+
                     imp.setImportLoja(getLojaOrigem());
                     imp.setImportSistema(getSistema());
                     imp.setImportId(rs.getString("id"));
@@ -168,23 +168,23 @@ public class ControlXDAO extends InterfaceDAO {
                     imp.setCodMercadologico1(rs.getString("merc1"));
                     imp.setCodMercadologico2(rs.getString("merc2"));
                     imp.setCodMercadologico3("1");
-                    
+
                     //Icms Débito
                     imp.setIcmsCstSaida(rs.getInt("cst"));
                     imp.setIcmsAliqSaida(rs.getDouble("icms_fora_estado"));
                     imp.setIcmsReducaoSaida(0);
-                    
+
                     //Icms Crédito
                     imp.setIcmsCstEntrada(rs.getInt("cst"));
                     imp.setIcmsAliqEntrada(rs.getDouble("icms_fora_estado"));
                     imp.setIcmsReducaoEntrada(0);
-                    
+
                     imp.setNcm(rs.getString("ncm"));
                     imp.setCest(rs.getString("cest"));
                     imp.setPiscofinsCstCredito(rs.getString("cst_cofins_credito"));
                     imp.setPiscofinsCstDebito(rs.getString("cst_cofins_debito"));
                     imp.setPiscofinsNaturezaReceita(rs.getString("naturezareceita"));
-                    
+
                     result.add(imp);
                 }
             }
@@ -195,35 +195,35 @@ public class ControlXDAO extends InterfaceDAO {
     @Override
     public List<FornecedorIMP> getFornecedores() throws Exception {
         List<FornecedorIMP> result = new ArrayList<>();
-        try(Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
-            try(ResultSet rs = stm.executeQuery(
-                    "select\n" +
-                    "	acesso id,\n" +
-                    "	situacao,\n" +
-                    "	cgc cnpj,\n" +
-                    "	insc_est,\n" +
-                    "	nome razao,\n" +
-                    "	NomeFantasia,\n" +
-                    "	endereco,\n" +
-                    "	bairro,\n" +
-                    "	cep,\n" +
-                    "	cidade,\n" +
-                    "	estado,\n" +
-                    "	fone,\n" +
-                    "	fax,\n" +
-                    "	contato,\n" +
-                    "	nPrazoEntr prazoentrega,\n" +
-                    "	cCondPagto condicaopagamento,\n" +
-                    "	cObs obs,\n" +
-                    "	cvendedor vendedor\n" +
-                    "from	\n" +
-                    "	TabFornecedor\n" +
-                    "order by	\n" +
-                    "	acesso"
+        try (Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "select\n"
+                    + "	acesso id,\n"
+                    + "	situacao,\n"
+                    + "	cgc cnpj,\n"
+                    + "	insc_est,\n"
+                    + "	nome razao,\n"
+                    + "	NomeFantasia,\n"
+                    + "	endereco,\n"
+                    + "	bairro,\n"
+                    + "	cep,\n"
+                    + "	cidade,\n"
+                    + "	estado,\n"
+                    + "	fone,\n"
+                    + "	fax,\n"
+                    + "	contato,\n"
+                    + "	nPrazoEntr prazoentrega,\n"
+                    + "	cCondPagto condicaopagamento,\n"
+                    + "	cObs obs,\n"
+                    + "	cvendedor vendedor\n"
+                    + "from	\n"
+                    + "	TabFornecedor\n"
+                    + "order by	\n"
+                    + "	acesso"
             )) {
-                while(rs.next()) {
+                while (rs.next()) {
                     FornecedorIMP imp = new FornecedorIMP();
-                    
+
                     imp.setImportLoja(getLojaOrigem());
                     imp.setImportSistema(getSistema());
                     imp.setImportId(rs.getString("id"));
@@ -238,29 +238,29 @@ public class ControlXDAO extends InterfaceDAO {
                     imp.setMunicipio(rs.getString("cidade"));
                     imp.setUf(rs.getString("estado"));
                     imp.setTel_principal(rs.getString("fone"));
-                    
-                    if(rs.getString("fax") != null && !"".equals(rs.getString("fax"))) {
+
+                    if (rs.getString("fax") != null && !"".equals(rs.getString("fax"))) {
                         imp.addContato("1", "FAX", rs.getString("fax"), null, TipoContato.NFE, null);
                     }
-                    
-                    if(rs.getString("contato") != null && !"".equals(rs.getString("contato"))) {
+
+                    if (rs.getString("contato") != null && !"".equals(rs.getString("contato"))) {
                         imp.addContato("2", rs.getString("contato"), null, null, TipoContato.NFE, null);
                     }
-                    
+
                     String vendedor = "";
-                    if(rs.getString("vendedor") != null && !"".equals(rs.getString("vendedor"))) {
+                    if (rs.getString("vendedor") != null && !"".equals(rs.getString("vendedor"))) {
                         vendedor = " Vendedor: " + rs.getString("vendedor");
                     }
-                    
+
                     imp.setObservacao(vendedor);
-                    
-                    if(rs.getString("obs") != null && !"".equals(rs.getString("obs"))) {
+
+                    if (rs.getString("obs") != null && !"".equals(rs.getString("obs"))) {
                         imp.setObservacao(rs.getString("obs") + vendedor);
                     }
-                    
+
                     imp.setPrazoEntrega(rs.getInt("prazoentrega"));
                     imp.addCondicaoPagamento(Utils.stringToInt(rs.getString("condicaopagamento")));
-                    
+
                     result.add(imp);
                 }
             }
@@ -271,27 +271,27 @@ public class ControlXDAO extends InterfaceDAO {
     @Override
     public List<ProdutoFornecedorIMP> getProdutosFornecedores() throws Exception {
         List<ProdutoFornecedorIMP> result = new ArrayList<>();
-        try(Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
-            try(ResultSet rs = stm.executeQuery(
-                    "select\n" +
-                    "	nAc_produto id_produto,\n" +
-                    "	nAc_Fornecedor id_fornecedor,\n" +
-                    "	cSituacao situacao,\n" +
-                    "	cRef_Fab codigoexterno\n" +
-                    "from	\n" +
-                    "	TabRelaci_ProdFornec\n" +
-                    "order by\n" +
-                    "	2, 1"
+        try (Statement stm = ConexaoSqlServer.getConexao().createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "select\n"
+                    + "	nAc_produto id_produto,\n"
+                    + "	nAc_Fornecedor id_fornecedor,\n"
+                    + "	cSituacao situacao,\n"
+                    + "	cRef_Fab codigoexterno\n"
+                    + "from	\n"
+                    + "	TabRelaci_ProdFornec\n"
+                    + "order by\n"
+                    + "	2, 1"
             )) {
-                while(rs.next()) {
+                while (rs.next()) {
                     ProdutoFornecedorIMP imp = new ProdutoFornecedorIMP();
-                    
+
                     imp.setImportLoja(getLojaOrigem());
                     imp.setImportSistema(getSistema());
                     imp.setIdProduto(rs.getString("id_produto"));
                     imp.setIdFornecedor(rs.getString("id_fornecedor"));
                     imp.setCodigoExterno(rs.getString("codigoexterno"));
-                    
+
                     result.add(imp);
                 }
             }
