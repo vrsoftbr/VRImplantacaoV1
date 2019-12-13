73454d75c0ecd399ab64a945f8c0a59d1e8c9d6d
Guilherme Gomes <guilherme.gomes@vrsoft.com.br>
2019-12-13 10:56:55 -0300

    Adicionado importação de produto fornecedor, cliente e crédito rotativo sistema Gestor PDV.

--- src/vrimplantacao2/dao/interfaces/GestorPdvDAO.java
+++ src/vrimplantacao2/dao/interfaces/GestorPdvDAO.java
@@ -6,10 +6,14 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import vrimplantacao.classe.ConexaoMySQL;
+import vrimplantacao.utils.Utils;
 import vrimplantacao2.dao.cadastro.Estabelecimento;
 import vrimplantacao2.vo.enums.TipoContato;
+import vrimplantacao2.vo.importacao.ClienteIMP;
+import vrimplantacao2.vo.importacao.CreditoRotativoIMP;
 import vrimplantacao2.vo.importacao.FornecedorIMP;
 import vrimplantacao2.vo.importacao.MercadologicoIMP;
+import vrimplantacao2.vo.importacao.ProdutoFornecedorIMP;
 import vrimplantacao2.vo.importacao.ProdutoIMP;
 
 /**
@@ -109,9 +113,9 @@ public class GestorPdvDAO extends InterfaceDAO {
                     imp.setImportLoja(getLojaOrigem());
                     imp.setImportSistema(getSistema());
                     imp.setImportId(rs.getString("id"));
-                    imp.setDescricaoCompleta(rs.getString("descricaocompleta"));
+                    imp.setDescricaoCompleta(Utils.acertarTexto(rs.getString("descricaocompleta")));
                     imp.setDescricaoGondola(imp.getDescricaoCompleta());
-                    imp.setDescricaoReduzida(rs.getString("descricaoreduzida"));
+                    imp.setDescricaoReduzida(Utils.acertarTexto(rs.getString("descricaoreduzida")));
                     imp.seteBalanca(rs.getInt("pesavel") == 1);
                     imp.setValidade(rs.getInt("validade"));
                     imp.setEan(rs.getString("codigobarras"));
@@ -230,5 +234,156 @@ public class GestorPdvDAO extends InterfaceDAO {
             }
         }
         return result;
-    }   
+    }
+
+    @Override
+    public List<ProdutoFornecedorIMP> getProdutosFornecedores() throws Exception {
+        List<ProdutoFornecedorIMP> result = new ArrayList<>();
+        
+        try(Statement stm = ConexaoMySQL.getConexao().createStatement()) {
+            try(ResultSet rs = stm.executeQuery(
+                    "select\n" +
+                    "  codigo idproduto,\n" +
+                    "  fornult1 idfornecedor\n" +
+                    "from\n" +
+                    "  produtos\n" +
+                    "where\n" +
+                    "  coalesce(fornult1, 0) != 0\n" +
+                    "order by\n" +
+                    "  2, 1")) {
+                while(rs.next()) {
+                    ProdutoFornecedorIMP imp = new ProdutoFornecedorIMP();
+                    
+                    imp.setImportLoja(getLojaOrigem());
+                    imp.setImportSistema(getSistema());
+                    imp.setIdProduto(rs.getString("idproduto"));
+                    imp.setIdFornecedor(rs.getString("idfornecedor"));
+                    
+                    result.add(imp);
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<ClienteIMP> getClientes() throws Exception {
+        List<ClienteIMP> result = new ArrayList<>();
+        
+        try(Statement stm = ConexaoMySQL.getConexao().createStatement()) {
+            try(ResultSet rs = stm.executeQuery(
+                    "select\n" +
+                    "  codigo id,\n" +
+                    "  nome razao,\n" +
+                    "  razao fantasia,\n" +
+                    "  identidade ie,\n" +
+                    "  cgc cnpj,\n" +
+                    "  cpf,\n" +
+                    "  endereco,\n" +
+                    "  bairro,\n" +
+                    "  cidade,\n" +
+                    "  numero,\n" +
+                    "  uf,\n" +
+                    "  cep,\n" +
+                    "  telefone,\n" +
+                    "  fax,\n" +
+                    "  celular,\n" +
+                    "  mail,\n" +
+                    "  datacad,\n" +
+                    "  contato,\n" +
+                    "  obs,\n" +
+                    "  ativo,\n" +
+                    "  limite,\n" +
+                    "  datanasc,\n" +
+                    "  saldodev,\n" +
+                    "  salario,\n" +
+                    "  limref limiteref\n" +
+                    "from\n" +
+                    "  clientes")) {
+                while(rs.next()) {
+                    ClienteIMP imp = new ClienteIMP();
+                    
+                    imp.setId(rs.getString("id"));
+                    imp.setRazao(Utils.acertarTexto(rs.getString("razao")));
+                    imp.setFantasia(rs.getString("fantasia"));
+                    imp.setInscricaoestadual(rs.getString("ie"));
+                    
+                    if(rs.getString("cnpj") != null && !"".equals(rs.getString("cnpj"))) {
+                        imp.setCnpj(rs.getString("cnpj"));
+                    } else {
+                        imp.setCnpj(rs.getString("cpf"));
+                    }
+                    
+                    imp.setEndereco(rs.getString("endereco"));
+                    imp.setBairro(rs.getString("bairro"));
+                    imp.setMunicipio(rs.getString("cidade"));
+                    imp.setNumero(rs.getString("numero"));
+                    imp.setUf(rs.getString("uf"));
+                    imp.setCep(rs.getString("cep"));
+                    imp.setTelefone(rs.getString("telefone"));
+                    imp.setCelular(rs.getString("celular"));
+                    imp.setFax(rs.getString("fax"));
+                    imp.setEmail(rs.getString("email"));
+                    imp.setDataCadastro(rs.getDate("datacad"));
+                    
+                    if(rs.getString("contato") != null && !"".equals(rs.getString("contato"))) {
+                        imp.addContato("1", rs.getString("contato"), null, null, null);
+                    }
+                    
+                    imp.setObservacao(rs.getString("obs"));
+                    imp.setAtivo(rs.getInt("ativo") == 1);
+                    imp.setValorLimite(rs.getDouble("limite"));
+                    imp.setDataNascimento(rs.getDate("datanasc"));
+                    
+                    result.add(imp);
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<CreditoRotativoIMP> getCreditoRotativo() throws Exception {
+        List<CreditoRotativoIMP> result = new ArrayList<>();
+        
+        try(Statement stm = ConexaoMySQL.getConexao().createStatement()) {
+            try(ResultSet rs = stm.executeQuery(
+                    "select\n" +
+                    "  numfinan id,\n" +
+                    "  numvenda cupom,\n" +
+                    "  codcli idcliente,\n" +
+                    "  documento,\n" +
+                    "  parcela,\n" +
+                    "  datalanc emissao,\n" +
+                    "  datavenc vencimento,\n" +
+                    "  valor,\n" +
+                    "  juros,\n" +
+                    "  multa,\n" +
+                    "  obs\n" +
+                    "from\n" +
+                    "  finan\n" +
+                    "where\n" +
+                    "  codcli > 0 and\n" +
+                    "  datapag = '0000-00-00'\n" +
+                    "order by\n" +
+                    "  datavenc")) {
+                while(rs.next()) {
+                    CreditoRotativoIMP imp = new CreditoRotativoIMP();
+                    
+                    imp.setId(rs.getString("id"));
+                    imp.setNumeroCupom(rs.getString("cupom"));
+                    imp.setIdCliente(rs.getString("idcliente"));
+                    imp.setParcela(rs.getInt("parcela"));
+                    imp.setDataEmissao(rs.getDate("emissao"));
+                    imp.setDataVencimento(rs.getDate("vencimento"));
+                    imp.setValor(rs.getDouble("valor"));
+                    imp.setJuros(rs.getDouble("juros"));
+                    imp.setMulta(rs.getDouble("multa"));
+                    
+                    result.add(imp);
+                }
+            }
+        }
+        return result;
+    }
 }
