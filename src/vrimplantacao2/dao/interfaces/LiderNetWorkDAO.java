a7ff2b319821260192757c676e0a23b2fb843ce1
Lucas Rafael <lucas.rafael@vrsoft.com.br>
2020-02-20 16:56:18 -0300

    Implementação dos métodos getProdutos(), getFornecedores e getLojas() LiderNetWorkDAO

--- src/vrimplantacao2/dao/interfaces/LiderNetWorkDAO.java
+++ src/vrimplantacao2/dao/interfaces/LiderNetWorkDAO.java
@@ -10,6 +10,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import vrimplantacao.classe.ConexaoFirebird;
+import vrimplantacao2.dao.cadastro.Estabelecimento;
+import vrimplantacao2.vo.importacao.FornecedorIMP;
 import vrimplantacao2.vo.importacao.ProdutoIMP;
 
 /**
@@ -21,6 +23,20 @@ public class LiderNetWorkDAO extends InterfaceDAO {
     @Override
     public String getSistema() {
         return "LiderNetWork";
+    }
+
+    public List<Estabelecimento> getLojaCliente() throws Exception {
+        List<Estabelecimento> result = new ArrayList<>();
+        try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "select cod_emp, razao_emp from empresa"
+            )) {
+                while (rs.next()) {
+                    result.add(new Estabelecimento(rs.getString("cod_emp"), rs.getString("razao_emp")));
+                }
+            }
+        }
+        return result;
     }
 
     @Override
@@ -47,6 +63,7 @@ public class LiderNetWorkDAO extends InterfaceDAO {
                     + "  p.valor_pro as precovenda,\n"
                     + "  p.valor_atacado as precoatacado,\n"
                     + "  p.preco_custo as custo,\n"
+                    + "  est.estoque,\n"
                     + "  p.estoque_minimo_pro as estoqueminimo,\n"
                     + "  p.ativo_pro as ativo,\n"
                     + "  p.margem_lucro_pro as margem,\n"
@@ -71,10 +88,45 @@ public class LiderNetWorkDAO extends InterfaceDAO {
                     + "left join grupo_icms icms_fora on icms_fora.cod_grp = p.icms_cont_fora\n"
                     + "left join grupo_icms icms_cf_est on icms_cf_est.cod_grp = p.icms_cf_est\n"
                     + "left join grupo_icms icms_cf_fora on icms_cf_fora.cod_grp = p.icms_cf_fora\n"
+                    + "left join estoque est on est.cod_pro = p.cod_produto_estoque\n"
+                    + "    and est.cod_emp = " + getLojaOrigem() + "\n"
                     + "order by p.cod_pro"
             )) {
                 while (rst.next()) {
                     ProdutoIMP imp = new ProdutoIMP();
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<FornecedorIMP> getFornecedores() throws Exception {
+        List<FornecedorIMP> result = new ArrayList<>();
+
+        try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "select\n"
+                    + "    f.cod_for as id,\n"
+                    + "    f.razao_for as razao,\n"
+                    + "    f.end_for as endereco,\n"
+                    + "    f.num_for as numero,\n"
+                    + "    f.bai_for as bairro,\n"
+                    + "    f.cid_for as cidade,\n"
+                    + "    f.codigo_ibge as cidade_ibge,\n"
+                    + "    f.cep_for as cep,\n"
+                    + "    f.est_for as estado,\n"
+                    + "    f.cnpj_for as cnpj,\n"
+                    + "    f.insc_for as ie_rg,\n"
+                    + "    f.tel_for as telefone,\n"
+                    + "    f.fax_for as fax,\n"
+                    + "    f.email_for as email,\n"
+                    + "    f.contato_for as contato\n"
+                    + "from fornecedor f\n"
+                    + "order by f.cod_for"
+            )) {
+                while (rst.next()) {
+                    FornecedorIMP imp = new FornecedorIMP();
                 }
             }
         }
