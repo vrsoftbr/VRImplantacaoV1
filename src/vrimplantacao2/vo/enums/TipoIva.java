10dd4d0aebb0d2e740d58f5a9d644a4e5647e8df
Leandro de Paula Caires <leandro.caires@vrsoft.com.br>
2020-03-11 17:37:25 -0300

    Melhoria na importação da Pauta Fiscal.

--- src/vrimplantacao2/vo/enums/TipoIva.java
+++ src/vrimplantacao2/vo/enums/TipoIva.java
@@ -1,5 +1,7 @@
 package vrimplantacao2.vo.enums;
 
+import vrimplantacao.utils.Utils;
+
 /**
  *
  * @author Leandro
@@ -7,6 +9,14 @@ package vrimplantacao2.vo.enums;
 public enum TipoIva {
     PERCENTUAL (0),
     VALOR (1);
+
+    public static TipoIva getByTipo(String tipoIva) {
+        switch (Utils.acertarTexto(tipoIva, 1)) {
+            case "V": return VALOR;
+            case "1": return VALOR;
+            default : return PERCENTUAL;
+        }
+    }
     
     private int id;
 
