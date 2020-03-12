494a7352ebc8d4f71538ee52b3f45e9b65da160e
Guilherme Gomes <guilherme.gomes@vrsoft.com.br>
2020-03-12 08:30:39 -0300

    Desenvolvimento de mercadólogico sistema W2A.

--- src/vrimplantacao2/dao/interfaces/W2ADAO.java
+++ src/vrimplantacao2/dao/interfaces/W2ADAO.java
@@ -5,9 +5,11 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import vrimplantacao.classe.ConexaoAccess;
+import vrimplantacao.utils.Utils;
 import vrimplantacao2.dao.cadastro.Estabelecimento;
 import vrimplantacao2.gui.component.mapatributacao.MapaTributoProvider;
 import vrimplantacao2.vo.importacao.MapaTributoIMP;
+import vrimplantacao2.vo.importacao.MercadologicoIMP;
 
 /**
  *
@@ -38,6 +40,48 @@ public class W2ADAO extends InterfaceDAO implements MapaTributoProvider {
             )) {
                 while (rst.next()) {
                     result.add(new Estabelecimento(rst.getString("id"), rst.getString("nome_fantasia")));
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    @Override
+    public List<MercadologicoIMP> getMercadologicos() throws Exception {
+        List<MercadologicoIMP> result = new ArrayList<>();
+        
+        try (Statement stm = ConexaoAccess.getConexao().createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "select \n" +
+                    " distinct\n" +
+                    " p.codigo_do_grupo as merc1,\n" +
+                    " g.nome_do_grupo as descmerc1,\n" +
+                    " s.codigo_subgrupo as merc2,\n" +
+                    " s.nome_subgrupo as descmerc2\n" +
+                    "from\n" +
+                    " produtos p,\n" +
+                    " grupos_de_produtos g,\n" +
+                    " subgrupo s\n" +
+                    "where\n" +
+                    "  p.codigo_do_grupo = g.codigo_do_grupo and\n" +
+                    "  p.codigo_subgrupo = s.codigo_subgrupo\n" +
+                    "order by\n" +
+                    "  2, 4"
+            )) {
+                while (rs.next()) {
+                    MercadologicoIMP imp = new MercadologicoIMP();
+                    
+                    imp.setImportSistema(getSistema());
+                    imp.setImportLoja(getLojaOrigem());                   
+                    imp.setMerc1ID(rs.getString("merc1"));
+                    imp.setMerc1Descricao(rs.getString("descmerc1"));
+                    imp.setMerc2ID(rs.getString("merc2"));
+                    imp.setMerc2Descricao(rs.getString("descmerc2"));
+                    imp.setMerc3ID(imp.getMerc1ID());
+                    imp.setMerc3Descricao(imp.getMerc2Descricao());
+                    
+                    result.add(imp);
                 }
             }
         }
