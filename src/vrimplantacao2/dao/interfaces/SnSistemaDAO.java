bde17b4e8db9bce4e15b201d3532247533288e3b
Leandro de Paula Caires <leandro.caires@vrsoft.com.br>
2020-03-12 17:11:42 -0300

    Inclusão do mercadológico no SnSistema.

--- src/vrimplantacao2/dao/interfaces/SnSistemaDAO.java
+++ src/vrimplantacao2/dao/interfaces/SnSistemaDAO.java
@@ -3,9 +3,14 @@ package vrimplantacao2.dao.interfaces;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import vrimplantacao.classe.ConexaoSqlServer;
 import vrimplantacao2.dao.cadastro.Estabelecimento;
+import vrimplantacao2.dao.cadastro.produto.OpcaoProduto;
+import vrimplantacao2.vo.importacao.MercadologicoIMP;
 
 /**
  *
@@ -47,6 +52,50 @@ public class SnSistemaDAO extends InterfaceDAO {
                             rs.getString("codigo"),
                             rs.getString("fantasia")
                     ));
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    @Override
+    public Set<OpcaoProduto> getOpcoesDisponiveisProdutos() {
+        return new HashSet<>(Arrays.asList(
+                OpcaoProduto.MERCADOLOGICO,
+                OpcaoProduto.MERCADOLOGICO_PRODUTO,
+                OpcaoProduto.MERCADOLOGICO_NAO_EXCLUIR
+        ));
+    }
+
+    @Override
+    public List<MercadologicoIMP> getMercadologicos() throws Exception {
+        List<MercadologicoIMP> result = new ArrayList<>();
+        
+        try (Statement st = ConexaoSqlServer.getConexao().createStatement()) {
+            try (ResultSet rs = st.executeQuery(
+                    "select\n" +
+                    "	dp.CODDEPARTAMENTO merc1,\n" +
+                    "	dp.DESCRICAO merc1_desc,\n" +
+                    "	sc.CODSECAO merc2,\n" +
+                    "	sc.DESCRICAO merc2_desc\n" +
+                    "from\n" +
+                    "	departamento dp\n" +
+                    "	join SECAO sc on\n" +
+                    "		sc.CODDEPARTAMENTO = dp.CODDEPARTAMENTO\n" +
+                    "order by\n" +
+                    "	dp.CODDEPARTAMENTO,\n" +
+                    "	sc.CODSECAO"
+            )) {
+                while (rs.next()) {
+                    MercadologicoIMP imp = new MercadologicoIMP();
+                    imp.setImportSistema(getSistema());
+                    imp.setImportLoja(getLojaOrigem());
+                    imp.setMerc1ID(rs.getString("merc1"));
+                    imp.setMerc1Descricao(rs.getString("merc1_desc"));
+                    imp.setMerc2ID(rs.getString("merc2"));
+                    imp.setMerc2Descricao(rs.getString("merc2_desc"));
+                    result.add(imp);
                 }
             }
         }
