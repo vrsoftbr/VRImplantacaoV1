b5369c0998216bb79fe828818baee5f40a9dbfa9
Guilherme Gomes <guilherme.gomes@vrsoft.com.br>
2020-04-17 10:20:52 -0300

    Adicionando sistema Logus Retaisl no MenuGUI. Pois em algum momento foi removido do Menu a opção.

--- src/vrimplantacao/gui/MenuGUI.java
+++ src/vrimplantacao/gui/MenuGUI.java
@@ -165,6 +165,7 @@ import vrimplantacao2.gui.interfaces.LinceGUI;
 import vrimplantacao2.gui.interfaces.LinnerGUI;
 import vrimplantacao2.gui.interfaces.LiteciGUI;
 import vrimplantacao2.gui.interfaces.LogTECGUI;
+import vrimplantacao2.gui.interfaces.LogusRetailGUI;
 import vrimplantacao2.gui.interfaces.LyncisGUI;
 import vrimplantacao2.gui.interfaces.MSIInforGUI;
 import vrimplantacao2.gui.interfaces.MarketGUI;
@@ -779,6 +780,8 @@ public final class MenuGUI extends VRMdiFrame {
         mnuContaPagar1 = new javax.swing.JMenuItem();
         mnuSQLite = new javax.swing.JMenu();
         mnuSQLiteSophyx = new javax.swing.JMenuItem();
+        mnuInformix = new javax.swing.JMenu();
+        mnuiLogus = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
         jMenuItem8 = new javax.swing.JMenuItem();
         jMenuItem24 = new javax.swing.JMenuItem();
@@ -2157,6 +2160,7 @@ public final class MenuGUI extends VRMdiFrame {
             }
         });
         jMenu8.add(mnuLogTec);
+
         mnuNCA.setLabel("NCA");
         mnuNCA.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -2646,6 +2650,18 @@ public final class MenuGUI extends VRMdiFrame {
         mnuSQLite.add(mnuSQLiteSophyx);
 
         mnuDatabase.add(mnuSQLite);
+
+        mnuInformix.setText("Informix");
+
+        mnuiLogus.setText("Logus Retail");
+        mnuiLogus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuiLogusActionPerformed(evt);
+            }
+        });
+        mnuInformix.add(mnuiLogus);
+
+        mnuDatabase.add(mnuInformix);
 
         mnuImpSistema.add(mnuDatabase);
 
@@ -4515,6 +4531,10 @@ public final class MenuGUI extends VRMdiFrame {
         NCAGUI.exibir(this);
     }//GEN-LAST:event_mnuNCAActionPerformed
 
+    private void mnuiLogusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuiLogusActionPerformed
+        LogusRetailGUI.exibir(this);
+    }//GEN-LAST:event_mnuiLogusActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSair;
     private javax.swing.JMenu jMenu10;
@@ -4685,6 +4705,7 @@ public final class MenuGUI extends VRMdiFrame {
     private javax.swing.JMenuItem mnuImportarNCM;
     private javax.swing.JMenuItem mnuImportarNfce;
     private javax.swing.JMenuItem mnuInfoMacStore;
+    private javax.swing.JMenu mnuInformix;
     private javax.swing.JMenuItem mnuIntellicon;
     private javax.swing.JMenuItem mnuInteragem;
     private javax.swing.JMenu mnuInterface;
@@ -4774,6 +4795,7 @@ public final class MenuGUI extends VRMdiFrame {
     private javax.swing.JMenuItem mnuWmsi;
     private javax.swing.JMenuItem mnuWmsi1;
     private javax.swing.JMenuItem mnuZpf;
+    private javax.swing.JMenuItem mnuiLogus;
     private javax.swing.JMenuItem mnuiSircom;
     private javax.swing.JMenuItem mnuiSophyx;
     private javax.swing.JMenuItem mnuiSysERP;
