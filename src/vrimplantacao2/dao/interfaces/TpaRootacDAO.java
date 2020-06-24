de2db381d3206d322ade78496e3a17b5817165bd
Leandro de Paula Caires <leandro.caires@vrsoft.com.br>
2020-06-24 12:59:28 -0300

    Inclusão da importação de Mercadológico no TpaRootac.

--- src/vrimplantacao2/dao/interfaces/TpaRootacDAO.java
+++ src/vrimplantacao2/dao/interfaces/TpaRootacDAO.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import vrframework.classe.ProgressBar;
 import vrimplantacao.classe.ConexaoSqlServer;
 import vrimplantacao.dao.cadastro.ProdutoBalancaDAO;
 import vrimplantacao.utils.Utils;
@@ -15,6 +16,8 @@ import vrimplantacao.vo.vrimplantacao.ProdutoBalancaVO;
 import vrimplantacao2.dao.cadastro.Estabelecimento;
 import vrimplantacao2.dao.cadastro.produto.OpcaoProduto;
 import vrimplantacao2.gui.component.mapatributacao.MapaTributoProvider;
+import vrimplantacao2.utils.multimap.MultiMap;
+import vrimplantacao2.vo.cadastro.mercadologico.MercadologicoNivelIMP;
 import vrimplantacao2.vo.importacao.FornecedorIMP;
 import vrimplantacao2.vo.importacao.MapaTributoIMP;
 import vrimplantacao2.vo.importacao.ProdutoIMP;
@@ -79,6 +82,126 @@ public class TpaRootacDAO extends InterfaceDAO implements MapaTributoProvider {
         }
         return result;
     }
+    
+    private void preencherNivel5(MercadologicoNivelIMP n4) throws Exception {
+        try (Statement st = ConexaoSqlServer.getConexao().createStatement();
+                ResultSet rs = st.executeQuery(
+                        "select\n" +
+                        "	GRUC03SETO merc1,\n" +
+                        "	GRUC03GRUP merc2,\n" +
+                        "	GRUC03SUBG merc3,\n" +
+                        "	GRUC03FAMI merc4,\n" +
+                        "	GRUC03SUBF merc5,\n" +
+                        "	GRUC35DESC descricao\n" +
+                        "from\n" +
+                        "	RC001GRU rg\n" +
+                        "where\n" +
+                        "	GRUC03SETO = '" + n4.getMercadologicoPai().getMercadologicoPai().getMercadologicoPai().getId() + "'\n" +
+                        "	and GRUC03GRUP = '" + n4.getMercadologicoPai().getMercadologicoPai().getId() + "'\n" +
+                        "	and GRUC03SUBG = '" + n4.getMercadologicoPai().getId() + "'\n" +
+                        "	and GRUC03FAMI = '" + n4.getId() + "'\n" +
+                        "	and GRUC03SUBF != ''"
+                )
+        ) {
+            while (rs.next()) {
+                n4.addFilho(rs.getString("merc5"), rs.getString("descricao"));
+            }
+        }
+    }
+    
+    private void preencherNivel4(MercadologicoNivelIMP n3) throws Exception {
+        try (Statement st = ConexaoSqlServer.getConexao().createStatement();
+                ResultSet rs = st.executeQuery(
+                        "select\n" +
+                        "	GRUC03SETO merc1,\n" +
+                        "	GRUC03GRUP merc2,\n" +
+                        "	GRUC03SUBG merc3,\n" +
+                        "	GRUC03FAMI merc4,\n" +
+                        "	GRUC35DESC descricao\n" +
+                        "from\n" +
+                        "	RC001GRU rg\n" +
+                        "where\n" +
+                        "	GRUC03SETO = '" + n3.getMercadologicoPai().getMercadologicoPai().getId() + "'\n" +
+                        "	and GRUC03GRUP = '" + n3.getMercadologicoPai().getId() + "'\n" +
+                        "	and GRUC03SUBG = '" + n3.getId() + "'\n" +
+                        "	and GRUC03FAMI != ''\n" +
+                        "	and GRUC03SUBF = ''"
+                )
+        ) {
+            while (rs.next()) {
+                preencherNivel5(n3.addFilho(rs.getString("merc4"), rs.getString("descricao")));
+            }
+        }
+    }
+    
+    private void preencherNivel3(MercadologicoNivelIMP n2) throws Exception {
+        try (Statement st = ConexaoSqlServer.getConexao().createStatement();
+                ResultSet rs = st.executeQuery(
+                        "select\n" +
+                        "	GRUC03SETO merc1,\n" +
+                        "	GRUC03GRUP merc2,\n" +
+                        "	GRUC03SUBG merc3,\n" +
+                        "	GRUC35DESC descricao\n" +
+                        "from\n" +
+                        "	RC001GRU rg\n" +
+                        "where\n" +
+                        "	GRUC03SETO = '" + n2.getMercadologicoPai().getId() + "'\n" +
+                        "	and GRUC03GRUP = '" + n2.getId() + "'\n" +
+                        "	and GRUC03SUBG != ''\n" +
+                        "	and GRUC03FAMI = ''"
+                )
+        ) {
+            while (rs.next()) {
+                preencherNivel4(n2.addFilho(rs.getString("merc3"), rs.getString("descricao")));
+            }
+        }
+    }
+    
+    private void preencherNivel2(MercadologicoNivelIMP n1) throws Exception {
+        try (Statement st = ConexaoSqlServer.getConexao().createStatement();
+                ResultSet rs = st.executeQuery(
+                        "select\n" +
+                        "	GRUC03SETO merc1,\n" +
+                        "	GRUC03GRUP merc2,\n" +
+                        "	GRUC35DESC descricao\n" +
+                        "from\n" +
+                        "	RC001GRU rg\n" +
+                        "where\n" +
+                        "	GRUC03SETO = '" + n1.getId() + "'\n" +
+                        "	and GRUC03GRUP != ''\n" +
+                        "	and GRUC03SUBG = ''"
+                )
+        ) {
+            while (rs.next()) {
+                preencherNivel3(n1.addFilho(rs.getString("merc2"), rs.getString("descricao")));
+            }
+        }
+    }
+
+    @Override
+    public List<MercadologicoNivelIMP> getMercadologicoPorNivel() throws Exception {
+        List<MercadologicoNivelIMP> result = new ArrayList<>();
+        
+        try (Statement st = ConexaoSqlServer.getConexao().createStatement()) {
+            try (ResultSet rs = st.executeQuery(
+                    "select GRUC03SETO merc1, GRUC35DESC descricao from RC001GRU rg where GRUC03GRUP = ''"
+            )) {
+                int count = 1;
+                while (rs.next()) {
+                    ProgressBar.setStatus("Carregando o mercadológico " + count);
+                    count++;
+                    MercadologicoNivelIMP n1 = new MercadologicoNivelIMP(
+                        rs.getString("merc1"),
+                        rs.getString("descricao")
+                    );
+                    preencherNivel2(n1);
+                    result.add(n1);
+                }
+            }
+        }
+        
+        return result;
+    }
 
     @Override
     public Set<OpcaoProduto> getOpcoesDisponiveisProdutos() {
@@ -97,7 +220,7 @@ public class TpaRootacDAO extends InterfaceDAO implements MapaTributoProvider {
                 OpcaoProduto.DESC_COMPLETA,
                 OpcaoProduto.DESC_GONDOLA,
                 OpcaoProduto.DESC_REDUZIDA,
-                OpcaoProduto.MERCADOLOGICO,
+                OpcaoProduto.MERCADOLOGICO_POR_NIVEL,
                 OpcaoProduto.MERCADOLOGICO_PRODUTO,
                 OpcaoProduto.MERCADOLOGICO_NAO_EXCLUIR,
                 OpcaoProduto.PESO_BRUTO,
@@ -139,7 +262,11 @@ public class TpaRootacDAO extends InterfaceDAO implements MapaTributoProvider {
                     "	0 validade,\n" +
                     "	p.PROCDESCRI descricaocompleta,\n" +
                     "	p.PROCDESRES descricaoreduzida,\n" +
-                    "	p.PROCCODGRU merc1,\n" +
+                    "	p.PROCCODSET merc1,	\n" +
+                    "	p.PROCCODGRU merc2,\n" +
+                    "	p.PROCCODSGR merc3,\n" +
+                    "	p.PROCCODFAM merc4,\n" +
+                    "	p.PROCCODSFA merc5,\n" +
                     "	p.PROCCODPAI id_familia,\n" +
                     "	p.PRONPESBRU pesobruto,\n" +
                     "	p.PRONPESLIQ pesoliquido,\n" +
@@ -157,7 +284,7 @@ public class TpaRootacDAO extends InterfaceDAO implements MapaTributoProvider {
                     "from\n" +
                     "	RC003EST p\n" +
                     "	join rc002loj lj on\n" +
-                    "		lj.lojc05codi = '00001'\n" +
+                    "		lj.lojc05codi = '" + getLojaOrigem() + "'\n" +
                     "	left join ean on\n" +
                     "		p.CODIGOPLU = ean.CODIGOPLU\n" +
                     "	left join RCEstEst est on\n" +
@@ -176,16 +303,30 @@ public class TpaRootacDAO extends InterfaceDAO implements MapaTributoProvider {
                     imp.setImportSistema(getSistema());
                     imp.setImportLoja(getLojaOrigem());
                     imp.setImportId(rst.getString("id"));
-                    imp.setEan(rst.getString("ean"));
-                    imp.setQtdEmbalagem(rst.getInt("qtdembalagem"));
+                    
+                    ProdutoBalancaVO bal = produtosBalanca.get(Utils.stringToInt(rst.getString("ean"), -2));
+                    if (bal != null) {
+                        imp.setQtdEmbalagem(1);
+                        imp.seteBalanca(true);
+                        imp.setEan(rst.getString("ean"));
+                        imp.setTipoEmbalagem("U".equals(bal.getPesavel()) ? "UN" : "KG");
+                        imp.setValidade(bal.getValidade());
+                    } else {                    
+                        imp.setQtdEmbalagem(rst.getInt("qtdembalagem"));
+                        imp.seteBalanca("S".equals(rst.getString("pesado")));
+                        imp.setEan(rst.getString("ean"));
+                        imp.setTipoEmbalagem(rst.getString("unidade"));
+                        imp.setValidade(rst.getInt("validade"));
+                    }
                     imp.setQtdEmbalagemCotacao(rst.getInt("qtdembalagemcotacao"));
-                    imp.setTipoEmbalagem(rst.getString("unidade"));
-                    imp.seteBalanca("S".equals(rst.getString("pesado")));
-                    imp.setValidade(rst.getInt("validade"));
                     imp.setDescricaoCompleta(rst.getString("descricaocompleta"));
                     imp.setDescricaoGondola(rst.getString("descricaocompleta"));
                     imp.setDescricaoReduzida(rst.getString("descricaoreduzida"));
                     imp.setCodMercadologico1(rst.getString("merc1"));
+                    imp.setCodMercadologico2(rst.getString("merc2"));
+                    imp.setCodMercadologico3(rst.getString("merc3"));
+                    imp.setCodMercadologico4(rst.getString("merc4"));
+                    imp.setCodMercadologico5(rst.getString("merc5"));
                     imp.setIdFamiliaProduto(rst.getString("id_familia"));
                     imp.setPesoBruto(rst.getDouble("pesobruto"));
                     imp.setPesoLiquido(rst.getDouble("pesoliquido"));
