1e89573959bff752b02304be45d23b25d353973b
Leandro de Paula Caires <leandro.caires@vrsoft.com.br>
2020-07-01 13:39:02 -0300

    Ajuste na ProdutoDAO.

--- src/vrimplantacao2/dao/cadastro/produto2/ProdutoDAO.java
+++ src/vrimplantacao2/dao/cadastro/produto2/ProdutoDAO.java
@@ -124,7 +124,7 @@ public class ProdutoDAO {
             sql.put("larguraembalagem", 0);
             sql.put("alturaembalagem", 0);
             sql.put("perda", 0.0);
-	    if (Versao.menorQue(3,21)) {
+	    if (Versao.menorQue(4)) {
                 sql.put("margemminima", vo.getMargemMinima());
                 sql.put("margemmaxima", vo.getMargemMaxima());
                 sql.put("margem", vo.getMargem());
