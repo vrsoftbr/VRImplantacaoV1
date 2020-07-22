dc2c97361bdcab6d7197b7ba6ee71c8a4f51f5bf
Leandro de Paula Caires <leandro.caires@vrsoft.com.br>
2020-07-22 17:15:45 -0300

    Inclusão do mercadológico do sistema VIGGO.

--- src/vrimplantacao2/dao/interfaces/ViggoDAO.java
+++ src/vrimplantacao2/dao/interfaces/ViggoDAO.java
@@ -1,12 +1,16 @@
 package vrimplantacao2.dao.interfaces;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import vrimplantacao.classe.ConexaoPostgres;
 import vrimplantacao2.dao.cadastro.Estabelecimento;
+import vrimplantacao2.dao.cadastro.produto.OpcaoProduto;
+import vrimplantacao2.vo.importacao.MercadologicoIMP;
 
 /**
  *
@@ -41,5 +45,59 @@ public class ViggoDAO extends InterfaceDAO {
         
         return result;
     }
+
+    @Override
+    public Set<OpcaoProduto> getOpcoesDisponiveisProdutos() {
+        return new HashSet<>(Arrays.asList(
+                OpcaoProduto.MAPA_TRIBUTACAO,
+                OpcaoProduto.IMPORTAR_MANTER_BALANCA,
+                OpcaoProduto.IMPORTAR_EAN_MENORES_QUE_7_DIGITOS,
+                OpcaoProduto.MERCADOLOGICO,
+                OpcaoProduto.MERCADOLOGICO_PRODUTO,
+                OpcaoProduto.MERCADOLOGICO_NAO_EXCLUIR
+        ));
+    }
+
+    @Override
+    public List<MercadologicoIMP> getMercadologicos() throws Exception {
+        List<MercadologicoIMP> result = new ArrayList<>();
+        
+        try (
+                Statement st = ConexaoPostgres.getConexao().createStatement();
+                ResultSet rs = st.executeQuery(
+                        "select\n" +
+                        "	distinct\n" +
+                        "	g.codigo merc1,\n" +
+                        "	g.nome merc1_desc,\n" +
+                        "	sg.codigo merc2,\n" +
+                        "	sg.nome merc2_desc\n" +
+                        "from\n" +
+                        "	produto p\n" +
+                        "	join grupo g on\n" +
+                        "		p.codigo_grupo = g.codigo\n" +
+                        "	left join subgrupo sg on\n" +
+                        "		p.codigo_subgrupo = sg.codigo\n" +
+                        "order by\n" +
+                        "	g.codigo"
+                )
+        ) {
+            while (rs.next()) {
+                MercadologicoIMP imp = new MercadologicoIMP();
+                
+                imp.setImportSistema(getSistema());
+                imp.setImportLoja(getLojaOrigem());
+                imp.setMerc1ID(rs.getString("merc1"));
+                imp.setMerc1Descricao(rs.getString("merc1_desc"));
+                //imp.setMerc2ID(rs.getString("merc2"));
+                //imp.setMerc2Descricao(rs.getString("merc2_desc"));
+                
+                result.add(imp);
+            }
+        }
+        
+        return result;
+    }
+    
+    
     
 }
