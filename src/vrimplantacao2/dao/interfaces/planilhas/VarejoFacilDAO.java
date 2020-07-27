6a87e3b9e6b0e050c47643684c750bf8ede32765
Alan Lyrio <alan.lyrio@vrsoft.com.br>
2020-07-27 16:02:30 -0300

    Adicionado VarejoFacilGUI no MenuGUI, desenvolvimento rotina para unificar fornecededor sistema Varejo Facil

--- src/vrimplantacao2/dao/interfaces/planilhas/VarejoFacilDAO.java
+++ src/vrimplantacao2/dao/interfaces/planilhas/VarejoFacilDAO.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,7 +18,10 @@ import jxl.Workbook;
 import jxl.WorkbookSettings;
 import vrframework.classe.ProgressBar;
 import vrimplantacao2.dao.interfaces.InterfaceDAO;
+import vrimplantacao2.vo.enums.TipoEmpresa;
+import vrimplantacao2.vo.enums.TipoFornecedor;
 import vrimplantacao2.vo.importacao.CreditoRotativoIMP;
+import vrimplantacao2.vo.importacao.FornecedorIMP;
 
 /**
  *
@@ -26,6 +30,7 @@ import vrimplantacao2.vo.importacao.CreditoRotativoIMP;
 public class VarejoFacilDAO extends InterfaceDAO {
 
     private String planilhaCreditoRotativo;
+    private String planilhaFornecedor;
 
     @Override
     public String getSistema() {
@@ -35,11 +40,87 @@ public class VarejoFacilDAO extends InterfaceDAO {
     public void setPlanilhaCreditoRotativo(String planilhaCreditoRotativo) {
         this.planilhaCreditoRotativo = planilhaCreditoRotativo;
     }
+    
+    public void setPlanilhaFornecedor(String planilhaFornecedor) {
+        this.planilhaFornecedor = planilhaFornecedor;
+    }
 
+    @Override
+    public List<FornecedorIMP> getFornecedores() throws Exception {
+        File file = new File(this.planilhaFornecedor);
+        List<FornecedorIMP> result = new ArrayList<>();
+        
+        if (file.exists()) {
+            WorkbookSettings settings = new WorkbookSettings();
+            settings.setEncoding("CP1250");
+            settings.setIgnoreBlanks(false);
+
+            Workbook planilha = Workbook.getWorkbook(file, settings);
+            Sheet sheet = planilha.getSheet(0);
+
+            ProgressBar.setStatus("Analisando Planilha de Fornecedores");
+            ProgressBar.setMaximum(sheet.getRows());
+            
+            int linha = 0;
+            try {
+                for (int i = 0; i < sheet.getRows(); i++) {
+                    Cell[] cells = sheet.getRow(i);
+
+                    linha++;
+
+                    if (linha == 1) {
+                        continue;
+                    }
+                    
+                    Cell cellId = sheet.getCell(0, i);
+                    Cell cellRazao = sheet.getCell(1, i);
+                    Cell cellCnpj = sheet.getCell(2, i);
+                    Cell cellTipo = sheet.getCell(3, i);
+                    Cell cellReigime = sheet.getCell(4, i);
+                    
+                    FornecedorIMP imp = new FornecedorIMP();
+                    imp.setImportLoja(getLojaOrigem());
+                    imp.setImportSistema(getSistema());
+                    imp.setImportId(cellId.getContents());
+                    imp.setRazao(cellRazao.getContents());
+                    imp.setFantasia(imp.getRazao());
+                    imp.setCnpj_cpf(cellCnpj.getContents());
+                    
+                    if (cellReigime.getContents().startsWith("1")) {
+                        imp.setTipoEmpresa(TipoEmpresa.LUCRO_REAL);
+                    } else if (cellReigime.getContents().startsWith("4")) {
+                        imp.setTipoEmpresa(TipoEmpresa.ME_SIMPLES);
+                    } else {
+                        imp.setTipoEmpresa(TipoEmpresa.LUCRO_REAL);
+                    }
+                    
+                    if ("DISTRIBUIDORA".equals(cellTipo.getContents())) {
+                        imp.setTipoFornecedor(TipoFornecedor.DISTRIBUIDOR);
+                    } else if ("INDUSTRIA".equals(cellTipo.getContents())) {
+                        imp.setTipoFornecedor(TipoFornecedor.INDUSTRIA);
+                    } else if ("VAREJO".equals(cellTipo.getContents())) {
+                        imp.setTipoFornecedor(TipoFornecedor.VAREJO);
+                    } else {
+                        imp.setTipoFornecedor(TipoFornecedor.INDUSTRIA);
+                    }
+                    
+                    result.add(imp);
+                    
+                }
+            } catch(Exception ex) {
+                System.out.println(linha);
+                throw ex;
+            }
+            return result;
+        } else {
+            throw new IOException("Planilha(s) não encontrada");
+        }
+    }
+    
     public List<CreditoRotativoIMP> getCreditoRotativo() throws Exception {
         File file = new File(this.planilhaCreditoRotativo);
         List<CreditoRotativoIMP> result = new ArrayList<>();
-        DateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
         java.sql.Date dataEmissao, dataVencimento;
 
         if (file.exists()) {
@@ -50,12 +131,12 @@ public class VarejoFacilDAO extends InterfaceDAO {
             Workbook planilha = Workbook.getWorkbook(file, settings);
             Sheet sheet = planilha.getSheet(0);
 
-            ProgressBar.setStatus("Analisando Planilha de Família de Produtos");
+            ProgressBar.setStatus("Analisando Planilha de Crédito Rotativo");
             ProgressBar.setMaximum(sheet.getRows());
 
             int linha = 0;
             try {
-                for (int i = 1; i < sheet.getRows(); i++) {
+                for (int i = 0; i < sheet.getRows(); i++) {
                     Cell[] cells = sheet.getRow(i);
 
                     linha++;
@@ -78,8 +159,15 @@ public class VarejoFacilDAO extends InterfaceDAO {
 
                     if ("A".equals(cellSituacao.getContents().trim())) {
                         CreditoRotativoIMP imp = new CreditoRotativoIMP();
-                        imp.setId(cellNumeroCupom.getContents() + "-" + cellDataEmissao.getContents().replace("/", "") + "-" + cellIdCliente.getContents());
+                        
+                        imp.setId(
+                                cellNumeroCupom.getContents() + "-"
+                                + cellDataEmissao.getContents().replace("/", "") + "-"
+                                + cellDataVencimento.getContents().replace("/", "") + "-"
+                                + cellIdCliente.getContents().trim());
+                        
                         imp.setIdCliente(cellIdCliente.getContents().trim());
+                        imp.setNumeroCupom(cellNumeroCupom.getContents());
                         imp.setDataEmissao(dataEmissao);
                         imp.setDataVencimento(dataVencimento);
                         imp.setValor(Double.parseDouble(cellValor.getContents().replace(".", "").replace(",", ".")));
