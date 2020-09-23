b22b5d26ac001b0a895eb10b4bd4bb102c7c096f
Guilherme Gomes <guilherme.gomes@vrsoft.com.br>
2020-09-23 14:20:36 -0300

    Iniciado desenvolvimento do sistema Solidos.

---
+++ src/vrimplantacao2/dao/interfaces/SolidoDAO.java
@@ -0,0 +1,140 @@
+package vrimplantacao2.dao.interfaces;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import vrimplantacao.classe.ConexaoFirebird;
+import vrimplantacao2.dao.cadastro.Estabelecimento;
+import vrimplantacao2.dao.cadastro.produto.OpcaoProduto;
+import vrimplantacao2.gui.component.mapatributacao.MapaTributoProvider;
+import vrimplantacao2.vo.importacao.FamiliaProdutoIMP;
+import vrimplantacao2.vo.importacao.MapaTributoIMP;
+
+/**
+ *
+ * @author Importacao
+ */
+public class SolidoDAO extends InterfaceDAO implements MapaTributoProvider {
+
+    @Override
+    public String getSistema() {
+        return "Solido";
+    }
+    
+    @Override
+    public Set<OpcaoProduto> getOpcoesDisponiveisProdutos() {
+        return new HashSet<>(Arrays.asList(
+                new OpcaoProduto[] {
+                    OpcaoProduto.MERCADOLOGICO,
+                    OpcaoProduto.MERCADOLOGICO_NAO_EXCLUIR,
+                    OpcaoProduto.MERCADOLOGICO_PRODUTO,
+                    OpcaoProduto.FAMILIA,
+                    OpcaoProduto.FAMILIA_PRODUTO,
+                    OpcaoProduto.IMPORTAR_MANTER_BALANCA,
+                    OpcaoProduto.PRODUTOS,
+                    OpcaoProduto.EAN,
+                    OpcaoProduto.EAN_EM_BRANCO,
+                    OpcaoProduto.IMPORTAR_EAN_MENORES_QUE_7_DIGITOS,
+                    OpcaoProduto.DATA_CADASTRO,
+                    OpcaoProduto.TIPO_EMBALAGEM_EAN,
+                    OpcaoProduto.TIPO_EMBALAGEM_PRODUTO,
+                    OpcaoProduto.PESAVEL,
+                    OpcaoProduto.VALIDADE,
+                    OpcaoProduto.DESC_COMPLETA,
+                    OpcaoProduto.DESC_GONDOLA,
+                    OpcaoProduto.DESC_REDUZIDA,
+                    OpcaoProduto.ESTOQUE_MAXIMO,
+                    OpcaoProduto.ESTOQUE_MINIMO,
+                    OpcaoProduto.PRECO,
+                    OpcaoProduto.CUSTO,
+                    OpcaoProduto.ESTOQUE,
+                    OpcaoProduto.ATIVO,
+                    OpcaoProduto.NCM,
+                    OpcaoProduto.CEST,
+                    OpcaoProduto.PIS_COFINS,
+                    OpcaoProduto.NATUREZA_RECEITA,
+                    OpcaoProduto.ICMS,
+                    OpcaoProduto.PAUTA_FISCAL,
+                    OpcaoProduto.PAUTA_FISCAL_PRODUTO,
+                    OpcaoProduto.MARGEM
+                }
+        ));
+    }
+
+    @Override
+    public List<MapaTributoIMP> getTributacao() throws Exception {
+        List<MapaTributoIMP> result = new ArrayList<>();
+        
+        try(Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try(ResultSet rs = stm.executeQuery(
+                    "select\n" +
+                    "	id_empresa_tributacao id,\n" +
+                    "	nome descricao,\n" +
+                    "	aliquota,\n" +
+                    "	sit_tributaria cst,\n" +
+                    "	reducao,\n" +
+                    "	aliquota_reduzida\n" +
+                    "from\n" +
+                    "	empresa_tributacao")) {
+                while(rs.next()) {
+                    result.add(new MapaTributoIMP(rs.getString("id"), 
+                                                rs.getString("descricao"), 
+                                                rs.getInt("cst"), 
+                                                rs.getDouble("aliquota"), 
+                                                rs.getDouble("reducao")));
+                }
+            }
+        }
+        return result;
+    }
+    
+    public List<Estabelecimento> getLojas() throws Exception {
+        List<Estabelecimento> lojas = new ArrayList<>();
+        try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "SELECT\n" +
+                    "	id_empresa id,\n" +
+                    "	NOME_FANTASIA fantasia\n" +
+                    "FROM\n" +
+                    "	EMPRESA"
+            )) {
+                while (rs.next()) {
+                    lojas.add(new Estabelecimento(rs.getString("id"), rs.getString("fantasia")));
+                }
+            }
+        }
+        return lojas;
+    }
+    
+    @Override
+    public List<FamiliaProdutoIMP> getFamiliaProduto() throws Exception {
+        List<FamiliaProdutoIMP> result = new ArrayList<>();
+
+        try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "select\n" +
+                    "	id_produto_familia id,\n" +
+                    "	nome descricao\n" +
+                    "from\n" +
+                    "	produto_familia"
+            )) {
+                while (rst.next()) {
+                    FamiliaProdutoIMP imp = new FamiliaProdutoIMP();
+                    
+                    imp.setImportSistema(getSistema());
+                    imp.setImportLoja(getLojaOrigem());
+                    imp.setImportId(rst.getString("id"));
+                    imp.setDescricao(rst.getString("descricao"));
+                    
+                    result.add(imp);
+                }
+            }
+        }
+
+        return result;
+    }
+}
