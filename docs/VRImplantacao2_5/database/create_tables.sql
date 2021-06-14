25253df05c9e0547f5163703e40cf42483da963d
Lucas Rafael <lucas.rafael@vrsoft.com.br>
2021-06-14 11:06:02 -0300

    Alteração no arquivo de diagrama de banco de dados do dia

--- docs/VRImplantacao2_5/database/create_tables.sql
+++ docs/VRImplantacao2_5/database/create_tables.sql
@@ -1,4 +1,6 @@
-﻿CREATE TABLE implantacao2_5.sistema(
+﻿CREATE SCHEMA implantacao2_5;
+
+CREATE TABLE implantacao2_5.sistema(
 	id serial PRIMARY KEY NOT NULL,
 	nome VARCHAR(60) NOT NULL
 );
@@ -12,16 +14,16 @@ CREATE TABLE implantacao2_5.sistemabancodados(
 	id serial PRIMARY KEY NOT NULL,
 	id_sistema INTEGER NOT NULL,
 	id_bancodados INTEGER NOT NULL,
+	nomeschema VARCHAR(60),
+	usuario VARCHAR(30),
+	senha VARCHAR(30),
 	CONSTRAINT fk_id_sistema FOREIGN KEY (id_sistema)
 		REFERENCES implantacao2_5.sistema (id),
 	CONSTRAINT fk_id_bancodados FOREIGN KEY (id_bancodados)
-		REFERENCES implantacao2_5.bancodados (id)
+		REFERENCES implantacao2_5.bancodados (id),
+	CONSTRAINT un_sistema_bancodados 
+		UNIQUE (id_sistema, id_bancodados)
 );
-
-ALTER TABLE implantacao2_5.sistemabancodados ADD CONSTRAINT un_sistema_bancodados UNIQUE (id_sistema, id_bancodados);
-ALTER TABLE implantacao2_5.sistemabancodados ADD nomeschema VARCHAR(60);
-ALTER TABLE implantacao2_5.sistemabancodados ADD usuario VARCHAR(30);
-ALTER TABLE implantacao2_5.sistemabancodados ADD senha VARCHAR(30);
 
 CREATE TABLE implantacao2_5.conexao(
 	id serial PRIMARY KEY NOT NULL,
@@ -32,5 +34,22 @@ CREATE TABLE implantacao2_5.conexao(
 	descricao VARCHAR (60),
 	id_sistemabancodados INTEGER NOT NULL,
 	CONSTRAINT fk_id_sistemabancodados FOREIGN KEY (id_sistemabancodados)
-	REFERENCES implantacao2_5.sistemabancodados (id)
+		REFERENCES implantacao2_5.sistemabancodados (id)
 );
+
+CREATE TABLE implantacao2_5.conexaoloja(
+	id serial PRIMARY KEY NOT NULL,
+	id_conexao INTEGER NOT NULL,
+	id_lojaorigem VARCHAR NOT NULL,
+	id_lojadestino INTEGER NOT NULL,
+	datacadastro TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+	id_situacaomigracao INTEGER NOT NULL,
+	lojamatriz BOOLEAN,
+	CONSTRAINT fk_if_conexao FOREIGN KEY (id_conexao)
+		REFERENCES implantacao2_5.conexao(id)
+);
+
+/*ALTER TABLE implantacao2_5.sistemabancodados ADD CONSTRAINT un_sistema_bancodados UNIQUE (id_sistema, id_bancodados);
+ALTER TABLE implantacao2_5.sistemabancodados ADD nomeschema VARCHAR(60);
+ALTER TABLE implantacao2_5.sistemabancodados ADD usuario VARCHAR(30);
+ALTER TABLE implantacao2_5.sistemabancodados ADD senha VARCHAR(30);*/
