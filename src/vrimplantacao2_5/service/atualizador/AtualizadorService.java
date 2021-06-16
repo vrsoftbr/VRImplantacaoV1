53661d1f3d297f827bf1352127df7d1404113261
Lucas Rafael <lucas.rafael@vrsoft.com.br>
2021-06-16 14:50:32 -0300

    Adicionado campo id na classe enum ESistema. Atualização nos scripts de create table na classe AtualizadorDAO

--- src/vrimplantacao2_5/service/atualizador/AtualizadorService.java
+++ src/vrimplantacao2_5/service/atualizador/AtualizadorService.java
@@ -74,5 +74,6 @@ public class AtualizadorService {
         this.atualizadorDAO.criarTabelas();
         this.inserirTabelaBancoDados();
         this.inserirTabelaSistema();
+        this.atualizadorDAO.inserirTabelaSistemaBancoDados();
     }
 }
