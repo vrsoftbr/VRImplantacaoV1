fd9714cfd3b6d02f23b9cab349191cb915ae0d7e
Lucas Rafael <lucas.rafael@vrsoft.com.br>
2021-08-16 10:50:36 -0300

    Desenvolvimento classe SistemaService

--- src/vrimplantacao2_5/dao/sistema/SistemaDAO.java
+++ src/vrimplantacao2_5/dao/sistema/SistemaDAO.java
@@ -6,35 +6,106 @@ import java.util.ArrayList;
 import java.util.List;
 import vrimplantacao2_5.vo.cadastro.SistemaVO;
 import vrframework.classe.Conexao;
+import vrimplantacao2.utils.sql.SQLBuilder;
+import vrimplantacao2_5.vo.cadastro.BancoDadosVO;
 
 /**
  *
  * @author guilhermegomes
  */
 public class SistemaDAO {
-    
+
     public List getSistema() throws Exception {
         List<SistemaVO> result = new ArrayList<>();
-        
-        try(Statement stm = Conexao.createStatement()) {
-            try(ResultSet rs = stm.executeQuery(
-                    "select \n" +
-                    "	*\n" +
-                    "from \n" +
-                    "	implantacao2_5.sistema\n" +
-                    "order by \n" +
-                    "	nome")) {
-                while(rs.next()) {
+
+        try (Statement stm = Conexao.createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "select \n"
+                    + "	*\n"
+                    + "from \n"
+                    + "	implantacao2_5.sistema\n"
+                    + "order by \n"
+                    + "	nome")) {
+                while (rs.next()) {
                     SistemaVO sistemaVO = new SistemaVO();
-                    
+
                     sistemaVO.setId(rs.getInt("id"));
                     sistemaVO.setNome(rs.getString("nome"));
-                    
+
                     result.add(sistemaVO);
                 }
             }
         }
-        
+
         return result;
+    }
+
+    public void salvar(SistemaVO vo) throws Exception {
+        SQLBuilder sql = new SQLBuilder();
+        sql.setSchema("implantacao2_5");
+        sql.setTableName("sistema");
+
+        sql.put("nome", vo.getNome());
+
+        sql.getReturning().add("id");
+
+        if (!sql.isEmpty()) {
+            try (Statement stm = Conexao.createStatement()) {
+                try (ResultSet rst = stm.executeQuery(sql.getInsert())) {
+                    vo.setId(rst.getInt("id"));
+                }
+            }
+        }
+    }
+    
+    public void alterar(BancoDadosVO vo) throws Exception {
+        SQLBuilder sql = new SQLBuilder();
+
+        sql.setTableName("sistema");
+        sql.setSchema("implantacao2_5");
+
+        sql.put("nome", vo.getNome());
+
+        sql.setWhere("id = " + vo.getId());
+
+        if (!sql.isEmpty()) {
+            try (Statement stm = Conexao.createStatement()) {
+                stm.execute(sql.getUpdate());
+            }
+        }        
+    }
+
+    public boolean existeSistema(String nome) throws Exception {
+        try (Statement stm = Conexao.createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "select nome\n"
+                    + "from implantacao2_5.sistema\n"
+                    + "where nome = '" + nome + "'"
+            )) {
+                return rst.next();
+            }
+        }
+    }    
+    
+    public List<BancoDadosVO> consultar() throws Exception {
+        List<BancoDadosVO> result = new ArrayList<>();
+
+        try (Statement stm = Conexao.createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "select\n"
+                    + "id,\n"
+                    + "nome\n"
+                    + "from implantacao2_5.sistema\n"
+                    + "order by 2"
+            )) {
+                while (rst.next()) {
+                    BancoDadosVO vo = new BancoDadosVO();
+                    vo.setId(rst.getInt("id"));
+                    vo.setNome(rst.getString("nome"));
+                    result.add(vo);
+                }
+            }
+            return result;
+        }
     }
 }
---
+++ src/vrimplantacao2_5/service/cadastro/SistemaService.java
@@ -0,0 +1,80 @@
+package vrimplantacao2_5.service.cadastro;
+
+import java.util.List;
+import vrframework.classe.Util;
+import vrframework.classe.VRException;
+import vrimplantacao2_5.dao.sistema.SistemaDAO;
+import vrimplantacao2_5.provider.ConexaoProvider;
+import vrimplantacao2_5.vo.cadastro.BancoDadosVO;
+import vrimplantacao2_5.vo.cadastro.SistemaVO;
+
+/**
+ *
+ * @author Desenvolvimento
+ */
+public class SistemaService {
+
+    private final ConexaoProvider provider;
+    private final SistemaDAO sistemaDAO;
+    
+    public SistemaService() {
+        this.sistemaDAO = new SistemaDAO();
+        this.provider = new ConexaoProvider();
+    }
+    
+    public SistemaService(SistemaDAO sistemaDAO, 
+                          ConexaoProvider provider) {
+        this.sistemaDAO = sistemaDAO;
+        this.provider = provider;
+    }
+    
+    public void existeBancoDados(String nome) throws Exception {
+        if (sistemaDAO.existeSistema(nome)) {
+            throw new VRException("Sistema já cadastrado");
+        }
+    }
+    
+    public void salvar(SistemaVO vo) throws Exception {
+        
+        try {
+            provider.begin();
+            
+            existeBancoDados(vo.getNome().trim());
+            sistemaDAO.salvar(vo);
+            
+            provider.commit();            
+        } catch (Exception ex) {
+            Util.exibirMensagemErro(ex, getTitle());
+            provider.rollback();
+        }
+    }
+    
+    public void alterar(BancoDadosVO vo) throws Exception {
+        try {
+            provider.begin();
+            
+            sistemaDAO.alterar(vo);
+            
+            provider.commit();
+        } catch(Exception ex) {
+            Util.exibirMensagemErro(ex, getTitle());
+            provider.rollback();            
+        }
+    }
+    
+    public List<BancoDadosVO> consultar() {
+        List<BancoDadosVO> result = null;
+        
+        try {
+            result = sistemaDAO.consultar();
+        } catch (Exception ex) {
+            Util.exibirMensagemErro(ex, "Consulta Banco de Dados");
+        }
+        
+        return result;
+    }
+    
+    private String getTitle() {
+        return "Cadastro Banco de Dados";
+    }    
+}
