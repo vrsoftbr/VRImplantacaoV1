afbce5aaa4eeb4194e5095f9792927a32a82d8a6
Lucas Rafael <lucas.rafael@vrsoft.com.br>
2021-08-16 16:02:17 -0300

    Desenvoilvimento classe SistemaBancoDadosVO

---
+++ src/vrimplantacao2_5/dao/cadastro/sistemabancodados/SistemaBancoDadosDAO.java
@@ -0,0 +1,100 @@
+package vrimplantacao2_5.dao.cadastro.sistemabancodados;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import vrframework.classe.Conexao;
+import vrimplantacao2.utils.sql.SQLBuilder;
+import vrimplantacao2_5.vo.cadastro.SistemaBancoDadosVO;
+
+/**
+ *
+ * @author Desenvolvimento
+ */
+public class SistemaBancoDadosDAO {
+
+    public void salvar(SistemaBancoDadosVO vo) throws Exception {
+
+        SQLBuilder sql = new SQLBuilder();
+        sql.setSchema("implantacao2_5");
+        sql.setTableName("sistemabancodados");
+
+        sql.put("id_sistema", vo.getIdSistema());
+        sql.put("id_bancodados", vo.getIdBancoDados());
+        sql.put("nomeschema", vo.getNomeSchema());
+        sql.put("usuario", vo.getUsuario());
+        sql.put("senha", vo.getSenha());
+        sql.put("porta", vo.getPorta());
+
+        sql.getReturning().add("id");
+
+        if (!sql.isEmpty()) {
+            try (Statement stm = Conexao.createStatement()) {
+                try (ResultSet rst = stm.executeQuery(sql.getInsert())) {
+                    if (rst.next()) {
+                        vo.setId(rst.getInt("id"));
+                    }
+                }
+            }
+        }
+    }
+
+    public void alterar(SistemaBancoDadosVO vo) throws Exception {
+
+        SQLBuilder sql = new SQLBuilder();
+        sql.setSchema("implantacao2_5");
+        sql.setTableName("sistemabancodados");
+
+        sql.put("id_sistema", vo.getIdSistema());
+        sql.put("id_bancodados", vo.getIdBancoDados());
+        sql.put("nomeschema", vo.getNomeSchema());
+        sql.put("usuario", vo.getUsuario());
+        sql.put("senha", vo.getSenha());
+        sql.put("porta", vo.getPorta());
+
+        sql.setWhere("id = " + vo.getId());
+
+        if (!sql.isEmpty()) {
+            try (Statement stm = Conexao.createStatement()) {
+                stm.execute(sql.getUpdate());
+            }
+        }
+    }
+
+    public List<SistemaBancoDadosVO> consultar(SistemaBancoDadosVO vo) throws Exception {
+        List<SistemaBancoDadosVO> result = new ArrayList<>();
+
+        try (Statement stm = Conexao.createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "select\n"
+                    + "	b.id as id_bancodados,\n"
+                    + "	b.nome as nome_bancodados,\n"
+                    + "	s.id as id_sistema,\n"
+                    + "	s.nome as nome_sistema,\n"
+                    + "	sb.nomeschema,\n"
+                    + "	sb.usuario,\n"
+                    + "	sb.senha,\n"
+                    + "	sb.porta\n"
+                    + "from implantacao2_5.sistemabancodados sb\n"
+                    + "join implantacao2_5.sistema s on s.id = sb.id_sistema\n"
+                    + "join implantacao2_5.bancodados b on b.id = sb.id_bancodados\n"
+                    + "order by b.nome, s.nome"
+            )) {
+                while (rst.next()) {
+                    SistemaBancoDadosVO sistemaBancoDadosVO = new SistemaBancoDadosVO();
+                    sistemaBancoDadosVO.setIdSistema(rst.getInt("id_sistema"));
+                    sistemaBancoDadosVO.setNomeSistema(rst.getString("nome_sistema"));
+                    sistemaBancoDadosVO.setIdBancoDados(rst.getInt("id_bancodados"));
+                    sistemaBancoDadosVO.setNomeBancoDados(rst.getString("nome_bancodados"));
+                    sistemaBancoDadosVO.setNomeSchema(rst.getString("nomeschema"));
+                    sistemaBancoDadosVO.setUsuario(rst.getString("usuario"));
+                    sistemaBancoDadosVO.setSenha(rst.getString("senha"));
+                    sistemaBancoDadosVO.setPorta(rst.getInt("porta"));
+                    result.add(vo);
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/vrimplantacao2_5/vo/cadastro/SistemaBancoDadosVO.java
@@ -0,0 +1,90 @@
+package vrimplantacao2_5.vo.cadastro;
+
+/**
+ *
+ * @author Desenvolvimento
+ */
+public class SistemaBancoDadosVO {
+
+    private int id;
+    private int idSistema;
+    private String nomeSistema;
+    private int idBancoDados;
+    private String nomeBancoDados;
+    private String nomeSchema;
+    private String usuario;
+    private String senha;
+    private int porta;
+    
+    public int getId() {
+        return this.id;
+    }
+    
+    public int getIdSistema() {
+        return this.idSistema;
+    }
+
+    public String getNomeSistema() {
+        return this.nomeSistema;
+    }
+    
+    public int getIdBancoDados() {
+        return this.idBancoDados;
+    }
+    
+    public String getNomeBancoDados() {
+        return this.nomeBancoDados;
+    }
+    
+    public String getNomeSchema() {
+        return this.nomeSchema;
+    }
+    
+    public String getUsuario() {
+        return this.usuario;
+    }
+    
+    public String getSenha() {
+        return this.senha;
+    }
+    
+    public int getPorta() {
+        return this.porta;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setIdSistema(int idSistema) {
+        this.idSistema = idSistema;
+    }
+    
+    public void setNomeSistema(String nomeSistema) {
+        this.nomeSistema = nomeSistema;
+    }
+    
+    public void setIdBancoDados(int idBancoDados) {
+        this.idBancoDados = idBancoDados;
+    }
+    
+    public void setNomeBancoDados(String nomeBancoDados) {
+        this.nomeBancoDados = nomeBancoDados;
+    }
+    
+    public void setNomeSchema(String nomeSchema) {
+        this.nomeSchema = nomeSchema;
+    }
+    
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public void setPorta(int porta) {
+        this.porta = porta;
+    }
+}
