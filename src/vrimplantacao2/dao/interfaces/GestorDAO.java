90e792686bbdd1bf0436a3ca81d79421ca4fce7e
wagner.simplicio <wagner.simplicio@vrsoft.com.br>
2021-10-18 15:04:50 -0300

    Inserção de cliente preferencial, Fornecedor e GetLoja

--- src/vrimplantacao2/dao/interfaces/GestorDAO.java
+++ src/vrimplantacao2/dao/interfaces/GestorDAO.java
@@ -2,14 +2,20 @@ package vrimplantacao2.dao.interfaces;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import vrimplantacao.classe.ConexaoFirebird;
+import vrimplantacao.utils.Utils;
+import vrimplantacao2.dao.cadastro.Estabelecimento;
 import vrimplantacao2.dao.cadastro.produto.OpcaoProduto;
 import vrimplantacao2.gui.component.mapatributacao.MapaTributoProvider;
+import vrimplantacao2.vo.importacao.ClienteIMP;
+import vrimplantacao2.vo.importacao.FornecedorIMP;
 import vrimplantacao2.vo.importacao.MapaTributoIMP;
 import vrimplantacao2.vo.importacao.MercadologicoIMP;
 import vrimplantacao2.vo.importacao.ProdutoIMP;
@@ -68,7 +74,33 @@ public class GestorDAO extends InterfaceDAO implements MapaTributoProvider {
                 OpcaoProduto.RECEITA
         ));
     }
-    
+
+    public List<Estabelecimento> getLojasCliente() throws Exception {
+        List<Estabelecimento> result = new ArrayList<>();
+
+        try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "SELECT \n"
+                    + " codigo AS codigo,\n"
+                    + " nome AS nome,\n"
+                    + " fantasia AS nomefantasia,\n"
+                    + " CNPJ_CPF  AS cpfcnpj\n"
+                    + "FROM ST_CD_EMPRESAS \n"
+                    + "ORDER BY 1"
+            )) {
+                while (rst.next()) {
+                    result.add(
+                            new Estabelecimento(
+                                    rst.getString("codigo"),
+                                    rst.getString("nomefantasia") + "-" + rst.getString("cpfcnpj")
+                            )
+                    );
+                }
+            }
+        }
+        return result;
+    }
+
     @Override
     public List<MapaTributoIMP> getTributacao() throws Exception {
         List<MapaTributoIMP> result = new ArrayList<>();
@@ -96,7 +128,7 @@ public class GestorDAO extends InterfaceDAO implements MapaTributoProvider {
         }
         return result;
     }
-    
+
     @Override
     public List<MercadologicoIMP> getMercadologicos() throws Exception {
         List<MercadologicoIMP> result = new ArrayList<>();
@@ -207,17 +239,190 @@ public class GestorDAO extends InterfaceDAO implements MapaTributoProvider {
                     imp.setPiscofinsCstDebito(rst.getString("piscofinssaida"));
                     imp.setPiscofinsCstCredito(rst.getString("pisconfinsentrada"));
                     imp.setPiscofinsNaturezaReceita(rst.getString("pis_nat_receita_tab"));
-                    
+
                     String idIcms = rst.getString("csticms") + "-" + rst.getString("aliqicms") + rst.getString("redicms");
-                    
+
                     imp.setIcmsDebitoId(idIcms);
                     imp.setIcmsDebitoForaEstadoId(idIcms);
                     imp.setIcmsDebitoForaEstadoNfId(idIcms);
                     imp.setIcmsCreditoId(idIcms);
                     imp.setIcmsCreditoForaEstadoId(idIcms);
                     imp.setIcmsConsumidorId(idIcms);
-                    
-                    result.add(imp);                    
+
+                    result.add(imp);
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<ClienteIMP> getClientes() throws Exception {
+        List<ClienteIMP> result = new ArrayList<>();
+        DateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+
+        try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try (ResultSet rs = stm.executeQuery(
+                    "SELECT \n"
+                    + " f.COD AS id,\n"
+                    + " f.PD_NOME AS razao,\n"
+                    + " f.PD_FANTASIA AS fantasia,\n"
+                    + " f.PD_CNPJ_CPF AS cnpj_cpf,\n"
+                    + " f.PD_IE AS ie_rg,\n"
+                    + " f.PD_ENDERECO AS endereco,\n"
+                    + " f.PD_NUMERO AS numero,\n"
+                    + " scc.NOME AS cidade,\n"
+                    + " scu.UF AS uf,\n"
+                    + " f.PD_CEP AS cep,\n"
+                    + " f.PD_BAIRRO AS bairro,\n"
+                    + " f.PD_COMPLEMENTO AS complemento,\n"
+                    + " f.PD_EMAIL AS email,\n"
+                    + " f.PD_MOVEL AS celular,\n"
+                    + " f.PD_FONE AS telefone1,\n"
+                    + " f.EC_ENDERECO AS c_endereco,\n"
+                    + " f.EC_BAIRRO AS c_bairro,\n"
+                    + " scc.NOME AS c_cidade,\n"
+                    + " f.EC_COMPLEMENTO AS c_complemento,\n"
+                    + " f.EC_CEP AS c_cep,\n"
+                    + " scu.UF AS c_uf,\n"
+                    + " f.EC_NUMERO AS c_numero,\n"
+                    + " f.OB_OBSERVACAO AS obs,\n"
+                    + " f.DATA_C AS dtcadastro,\n"
+                    + " f.PD_DTANASCCONST AS dtnascimento,\n"
+                    + " scmmc.IP_PAI AS pai,\n"
+                    + " scmmc.IP_MAE AS mae,\n"
+                    + " scmmc.IP_CONJ_NOME AS conjuge,\n"
+                    + " scmmc.IC_CO_LIMITE AS limite,\n"
+                    + " scmmc.CO_SALARIO AS salario,\n"
+                    + " scmmc.CO_EMPRESA AS empresa,\n"
+                    + " scmmc.CO_CARGO AS profissao,\n"
+                    + " CASE WHEN scms.DSC <> 'ATIVO' THEN 0\n"
+                    + " ELSE 1\n"
+                    + " END AS status,\n"
+                    + " CASE WHEN scms.BLOQ <> 0 THEN 0\n"
+                    + " ELSE 1 \n"
+                    + " END AS bloqueado\n"
+                    + "FROM SM_CD_MO_MOVIMENTADOR f\n"
+                    + "LEFT JOIN ST_CD_CIDADES scc ON scc.CODIGO = f.PD_CIDADE \n"
+                    + "LEFT JOIN ST_CD_UF scu ON scu.UF = scc.UF \n"
+                    + "LEFT JOIN SM_CD_MO_SITUACAO scms ON scms.COD = f.PD_SITUACAO\n"
+                    + "LEFT JOIN SM_CD_MO_MOVIMENTADOR_CL scmmc ON scmmc.COD = f.COD\n"
+                    + "WHERE PD_CNPJ_CPF_TIPO = 1\n"
+                    + "ORDER BY 1"
+            )) {
+                while (rs.next()) {
+                    ClienteIMP imp = new ClienteIMP();
+
+                    imp.setId(rs.getString("id"));
+                    imp.setRazao(rs.getString("razao"));
+                    imp.setFantasia(rs.getString("fantasia"));
+
+                    imp.setDataNascimento(rs.getDate("dtnascimento"));
+
+                    imp.setCnpj(rs.getString("cpfcnpj"));
+                    imp.setInscricaoestadual(rs.getString("inscricao"));
+
+                    imp.setTelefone(rs.getString("telefone1"));
+                    //imp.setFax(rs.getString("telefone2"));
+                    imp.setCelular(rs.getString("celular"));
+                    imp.setEmail(rs.getString("email"));
+                    imp.setDataCadastro(rs.getDate("datacadastro"));
+                    imp.setBloqueado(rs.getBoolean("bloqueado"));
+                    imp.setAtivo(rs.getBoolean("status"));
+                    imp.setObservacao(rs.getString("obs"));
+
+                    imp.setEndereco(rs.getString("endereco"));
+                    imp.setNumero(rs.getString("numero"));
+                    imp.setComplemento(rs.getString("complemento"));
+                    imp.setBairro(rs.getString("bairro"));
+                    imp.setCep(rs.getString("cep"));
+                    imp.setMunicipio(rs.getString("cidade"));
+                    imp.setUf(rs.getString("uf"));
+
+                    imp.setCobrancaEndereco(rs.getString("c_endereco"));
+                    imp.setCobrancaNumero(rs.getString("c_numero"));
+                    imp.setCobrancaComplemento(rs.getString("c_complemento"));
+                    imp.setCobrancaBairro(rs.getString("c_bairro"));
+                    imp.setCobrancaCep(rs.getString("c_cep"));
+                    imp.setCobrancaMunicipio(rs.getString("c_cidade"));
+                    imp.setCobrancaUf(rs.getString("c_uf"));
+
+                    imp.setNomePai(rs.getString("pai"));
+                    imp.setNomeMae(rs.getString("mae"));
+                    imp.setNomeConjuge(rs.getString("conjuge"));
+                    imp.setCargo(rs.getString("profissao"));
+                    imp.setSalario(rs.getDouble("salario"));
+                    imp.setValorLimite(rs.getDouble("valorlimite"));
+
+                    result.add(imp);
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<FornecedorIMP> getFornecedores() throws Exception {
+        List<FornecedorIMP> result = new ArrayList<>();
+
+        try (Statement stm = ConexaoFirebird.getConexao().createStatement()) {
+            try (ResultSet rst = stm.executeQuery(
+                    "SELECT \n"
+                    + " f.COD AS fornecedorid,\n"
+                    + " f.PD_NOME AS razao,\n"
+                    + " f.PD_FANTASIA AS fantasia,\n"
+                    + " f.PD_CNPJ_CPF AS cnpj_cpf,\n"
+                    + " f.PD_IE AS ie_rg,\n"
+                    + " f.PD_ENDERECO AS endereco,\n"
+                    + " f.PD_NUMERO AS numero,\n"
+                    + " scc.NOME AS municipio,\n"
+                    + " scu.UF AS uf,\n"
+                    + " f.PD_CEP AS cep,\n"
+                    + " f.PD_BAIRRO AS bairro,\n"
+                    + " f.PD_COMPLEMENTO AS complemento,\n"
+                    + " f.PD_EMAIL AS email,\n"
+                    + " f.PD_MOVEL AS cel,\n"
+                    + " f.PD_FONE AS tel,\n"
+                    + " f.OB_OBSERVACAO as observacao,\n"
+                    + " f.PD_DATA,\n"
+                    + " f.DATA_C as dtcadastro,\n"
+                    + " CASE WHEN scms.DSC <> 'ATIVO' THEN 0\n"
+                    + " ELSE 1\n"
+                    + " END AS status,\n"
+                    + " CASE WHEN scms.BLOQ <> 0 THEN 0\n"
+                    + " ELSE 1 \n"
+                    + " END AS bloqueado\n"
+                    + "FROM SM_CD_MO_MOVIMENTADOR f\n"
+                    + "LEFT JOIN ST_CD_CIDADES scc ON scc.CODIGO = f.PD_CIDADE \n"
+                    + "LEFT JOIN ST_CD_UF scu ON scu.UF = scc.UF \n"
+                    + "LEFT JOIN SM_CD_MO_SITUACAO scms ON scms.COD = f.PD_SITUACAO\n"
+                    + "WHERE PD_CNPJ_CPF_TIPO = 0\n"
+                    + "ORDER BY 1"
+            )) {
+                while (rst.next()) {
+                    FornecedorIMP imp = new FornecedorIMP();
+                    imp.setImportLoja(getLojaOrigem());
+                    imp.setImportSistema(getSistema());
+
+                    imp.setImportId(rst.getString("fornecedorid"));
+                    imp.setRazao(rst.getString("razao"));
+                    imp.setFantasia(rst.getString("fantasia"));
+                    imp.setCnpj_cpf(rst.getString("cnpj_cpf"));
+                    imp.setIe_rg(rst.getString("ie_rg"));
+                    imp.setAtivo(rst.getBoolean("status"));
+                    imp.setEndereco(rst.getString("endereco"));
+                    imp.setNumero(rst.getString("numero"));
+                    imp.setComplemento(rst.getString("complemento"));
+                    imp.setBairro(rst.getString("bairro"));
+                    imp.setCep(rst.getString("cep"));
+                    imp.setMunicipio(rst.getString("municipio"));
+                    imp.setUf(rst.getString("uf"));
+                    imp.setTel_principal(rst.getString("tel"));
+                    imp.setDatacadastro(rst.getDate("dtcadastro"));
+                    imp.setObservacao(rst.getString("observacao"));
+                    imp.setBloqueado(rst.getBoolean("bloqueado"));
+
+                    result.add(imp);
                 }
             }
         }
