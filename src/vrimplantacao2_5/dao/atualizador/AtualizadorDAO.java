8c9306e9c364c95dd0b4aa2bf7cc659e1e07b8b0
Alan Lyrio <alan.lyrio@vrsoft.com.br>
2022-04-19 14:36:23 -0300

    Alterado dataprocessamentoDAO, fixado loja 1 no rst, alterados inserts de clientes e unidades no AtualizadorDAO

--- src/vrimplantacao2_5/dao/atualizador/AtualizadorDAO.java
+++ src/vrimplantacao2_5/dao/atualizador/AtualizadorDAO.java
@@ -65,7 +65,7 @@ public class AtualizadorDAO {
     public void criarCampoDataImportacao() throws Exception {
         try (Statement stm = Conexao.createStatement()) {
             stm.execute(
-                     "alter table implantacao.codant_clientepreferencial \n"
+                    "alter table implantacao.codant_clientepreferencial \n"
                     + "add column if not exists dataimportacao timestamp default now();\n"
                     + "alter table implantacao.codant_clienteeventual \n"
                     + "add column if not exists dataimportacao timestamp default now();\n"
@@ -314,22 +314,16 @@ public class AtualizadorDAO {
 
     public void inserirUnidade() throws Exception {
         try (Statement stm = Conexao.createStatement()) {
-            stm.execute("DELETE FROM implantacao2_5.usuario;\n"
-                    + "DELETE FROM implantacao2_5.unidade\n;"
-                    + "INSERT INTO implantacao2_5.unidade(id, nome, id_municipio, id_estado) VALUES (1, 'VR MATRIZ', 3526902, 35);\n"
-                    + "INSERT INTO implantacao2_5.unidade(id, nome, id_municipio, id_estado) VALUES (6, 'VR GOIANIA', 5208707, 52);");
+            stm.execute("INSERT INTO implantacao2_5.unidade(id, nome, id_municipio, id_estado) VALUES (1, 'VR MATRIZ', 3526902, 35) on conflict (id) do nothing;\n");
         }
     }
 
     public void inserirUsuario() throws Exception {
         try (Statement stm = Conexao.createStatement()) {
-            stm.execute("DELETE FROM implantacao2_5.usuario;\n"
-                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (1, 'GUILHERME', 'GUILHERME', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ");\n"
-                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (2, 'LUCAS', 'LUCAS', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ");\n"
-                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (3, 'ALAN', 'ALAN', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ");\n"
-                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (4, 'WAGNER', 'WAGNER', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ");\n"
-                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (5, 'MICHAEL', 'MICHAEL', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ");\n"
-                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (6, 'JOSE', 'JOSE', 'VR123', " + EUnidade.VR_GOIANIA.getId() + ");");
+            stm.execute("INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (1, 'GUILHERME', 'GUILHERME', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ") ON CONFLICT (id) DO NOTHING;\n"
+                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (2, 'ALAN', 'ALAN', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ") ON CONFLICT (id) DO NOTHING;\n"
+                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (3, 'WAGNER', 'WAGNER', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ") ON CONFLICT (id) DO NOTHING;\n"
+                    + "INSERT INTO implantacao2_5.usuario(id, nome, login, senha, id_unidade) VALUES (4, 'MICHAEL', 'MICHAEL', 'ZIRDA123', " + EUnidade.VR_MATRIZ.getId() + ") ON CONFLICT (id) DO NOTHING;\n");
         }
     }
 
